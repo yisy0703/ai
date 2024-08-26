d2f727e146b0a24d1ee2347ec24ed2471a56f75f
yisy0703 <yisy7003@naver.com>
2024-08-26 15:25:38 +0900

    Java variable

--- source/01_java/ch01/src/ch01/Hello.java
+++ source/01_java/ch01/src/ch01/Hello.java
@@ -1,7 +1,10 @@
 package ch01;
-
+// 한줄 주석
 public class Hello {
 	public static void main(String[] args) {
+		/* System은 Java에서 제공하는 꾸러미(API=라이브러리)
+		 * dot(.) : System이라는 꾸러미 안의 out
+		 */
 		System.out.println("Hello, Java");
 		System.out.println('H');
 	}
