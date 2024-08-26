34b67d596c9235e01b7bfd53838fe8718c4821b7
yisy0703 <yisy7003@naver.com>
2024-08-26 16:17:34 +0900

    Java variable

---
+++ source/01_java/ch02_variable/src/com/lec/ex/VarEx01.java
@@ -0,0 +1,16 @@
+package com.lec.ex;
+
+public class VarEx01 {
+	public static void main(String[] args) {
+		// 변수타입 변수명 = 값;
+		int age = 27; // 4byte 정수
+		// age = 27.5; 변수 타입과 다른 값
+		String name = "제니";
+		name = "홍길동";
+		System.out.println("안녕하세요. " + age + "살 "+ name +"씨");// ctrl + alt + ↓ (한줄복사)
+		System.out.println("안녕하세요. " + age + "살 "+ name +"씨");// ctrl + alt + ↓ (한줄복사)
+		System.out.println("안녕하세요. " + age + "살 "+ name +"씨");// ctrl + alt + ↓ (한줄복사)
+		System.out.println("안녕하세요. " + age + "살 "+ name +"씨");// ctrl + alt + ↓ (한줄복사)
+		System.out.println("안녕하세요. " + age + "살 "+ name +"씨");// ctrl + alt + ↓ (한줄복사)
+	}
+}
