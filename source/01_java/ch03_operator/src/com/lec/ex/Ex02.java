094ae54a579f1896e9ff61f5f096eba50043e2d1
yisy0703 <yisy7003@naver.com>
2024-08-27 11:56:04 +0900

    Java operation

--- source/01_java/ch03_operator/src/com/lec/ex/Ex02.java
+++ source/01_java/ch03_operator/src/com/lec/ex/Ex02.java
@@ -5,5 +5,7 @@ public class Ex02 {
 		int n1 = 10;
 		int n2 = ++n1; // n1을 1증가하고, n2에 할당 (둘다 11)
 		System.out.println("n1=" + n1 + ", n2=" + n2);
+		n2 = n1++ ; // n1값을 n2에 할당한 후 n1을 1증가 (n2=11, n1=12)
+		System.out.println("n1=" + n1 + ", n2=" + n2);
 	}
 }
