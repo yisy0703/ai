fd74805076b443e09c3daae719545447f1620cd8
yisy0703 <yisy7003@naver.com>
2024-08-27 17:32:23 +0900

    Java if

--- source/01_java/ch04_control/src/com/lec/condition/Ex03_switch.java
+++ source/01_java/ch04_control/src/com/lec/condition/Ex03_switch.java
@@ -2,7 +2,21 @@ package com.lec.condition;
 public class Ex03_switch {
 	public static void main(String[] args) {
 		int num=3;
-		
+		// switch(정수형이나 문자형이나 문자열만 가능)
+		switch(num) {
+		case 1:
+			System.out.println("주사위 1"); break;
+		case 2:
+			System.out.println("주사위 2"); break;
+		case 3:
+			System.out.println("주사위 3"); break;
+		case 4:
+			System.out.println("주사위 4"); break;
+		case 5:
+			System.out.println("주사위 5"); break; 
+		default :
+			System.out.println("주사위 6");
+		}
 //		if(num==1) {
 //			System.out.println("주사위 1");
 //		}else if(num==2) {
---
+++ source/01_java/ch04_control/src/com/lec/condition/Ex04_if_hakjum.java
@@ -0,0 +1,30 @@
+package com.lec.condition;
+import java.util.Scanner;
+// 점수(0~100점)를 입력받아 학점 출력
+public class Ex04_if_hakjum {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("점수는?");
+		int score = scanner.nextInt();
+		if( 90 <= score && score <= 100 ) {
+			System.out.println("A학점");
+		}else if( 80 <= score && score < 90) {
+			System.out.println("B학점");
+		}else if( 70 <= score && score < 80) {
+			System.out.println("C학점");
+		}else if( 60 <= score && score < 70) {
+			System.out.println("D학점");
+		}else if( 0 <= score && score < 60) {
+			System.out.println("F학점");
+		}else {
+			System.out.println("유효하지 않은 점수입니다");
+		}
+		scanner.close();
+	}
+}
+
+
+
+
+
+
