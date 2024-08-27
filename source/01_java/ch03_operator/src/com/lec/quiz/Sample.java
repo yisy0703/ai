cfa55a085d1af46fed04d6fe8adb0500e7edf792
yisy0703 <yisy7003@naver.com>
2024-08-27 14:42:37 +0900

    Java operation

--- source/01_java/ch03_operator/src/com/lec/quiz/Sample.java
+++ source/01_java/ch03_operator/src/com/lec/quiz/Sample.java
@@ -1,8 +1,12 @@
 package com.lec.quiz;
+import java.util.Scanner;
 // 사용자로부터 입력받은 수가 3의 배수인지 아닌지 출력
 public class Sample {
 	public static void main(String[] args) {
-		String name = new String("Hong");
-		System.out.println(name.toUpperCase());
+		//String name = new String("Hong");
+		//System.out.println(name.toUpperCase());
+		Scanner sc = new Scanner(System.in); //
+		
+		int su = sc.nextInt(); // 키보드로부터 입력받은 정수를 반환하는 기능
 	}
 }
