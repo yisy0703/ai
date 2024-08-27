284df23564732392d43c8637b2bff87d3e76d7e4
yisy0703 <yisy7003@naver.com>
2024-08-27 10:51:11 +0900

    Java operation

---
+++ source/01_java/ch02_variable/src/com/lec/ex/VarEx06.java
@@ -0,0 +1,14 @@
+package com.lec.ex;
+
+public class VarEx06 {
+	public static void main(String[] args) {
+		// 기초데이터 타입
+		int i;
+		i = 10;
+		char gender = '남';
+		// 참조데이터 타입(객체 데이터 타입)
+		String name = "Hong";
+		name = "김수한무거북이와두루미";
+		i = 100;
+	}
+}
---
+++ source/01_java/ch02_variable/src/com/lec/quiz/Example.java
@@ -0,0 +1,15 @@
+package com.lec.quiz;
+// 국어, 영어, 수학 점수를 할당하고, 각 점수와 총점을 출력
+public class Example {
+	public static void main(String[] args) {
+//		int kor = 100, eng = 90, mat = 100; // 국, 영, 수 
+		int kor = 100;
+		int eng = 90;
+		int mat = 100;
+		int tot = kor + eng + mat; // tot = 290;
+		System.out.println("국어 : " + kor + "\t 영어 : " + eng + "\t 수학 : " + mat);
+		System.out.println("총점 : " + tot);		
+		System.out.println("총점 : " + tot);		
+		
+	}
+}
