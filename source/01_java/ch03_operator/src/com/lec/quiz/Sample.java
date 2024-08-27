3938e3b49e0c1f73d7f646f159ff690ca96db9e9
yisy0703 <yisy7003@naver.com>
2024-08-27 14:28:21 +0900

    Java operation

--- source/01_java/ch03_operator/src/com/lec/quiz/Sample.java
+++ source/01_java/ch03_operator/src/com/lec/quiz/Sample.java
@@ -1,5 +1,8 @@
 package com.lec.quiz;
-
+// 사용자로부터 입력받은 수가 3의 배수인지 아닌지 출력
 public class Sample {
-
+	public static void main(String[] args) {
+		String name = new String("Hong");
+		System.out.println(name.toUpperCase());
+	}
 }
