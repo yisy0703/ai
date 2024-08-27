062f01d2ee87e5a2693f1b38b91ba07e30279640
yisy0703 <yisy7003@naver.com>
2024-08-27 12:06:37 +0900

    Java operation

--- source/01_java/ch03_operator/src/com/lec/ex/Ex03.java
+++ source/01_java/ch03_operator/src/com/lec/ex/Ex03.java
@@ -5,6 +5,6 @@ public class Ex03 {
 		int n1=10, n2=5;
 		boolean result;
 		result = n1>=n2;
-		System.out.printf("%d %s %d = %b\n", n1, )
+		System.out.printf("%d %s %d = %b\n", n1, ">=", n2, result);
 	}
 }
