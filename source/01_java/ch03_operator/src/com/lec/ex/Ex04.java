b9c9f702c8e209f2a12f36c6028ded3cfd9da1a9
yisy0703 <yisy7003@naver.com>
2024-08-27 12:17:12 +0900

    Java operation

--- source/01_java/ch03_operator/src/com/lec/ex/Ex04.java
+++ source/01_java/ch03_operator/src/com/lec/ex/Ex04.java
@@ -1,5 +1,15 @@
 package com.lec.ex;
-// 논리연산자
+// 논리연산자 : &&(and), ||(or), !(not)
+/* true && true => true
+ * true && false
+ * false && true
+ * false && false
+ */
+/* true || true => true
+ * true || false
+ * false || true
+ * false || false =>false
+ */
 public class Ex04 {
 
 }
