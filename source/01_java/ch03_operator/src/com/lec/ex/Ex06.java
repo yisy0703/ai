46707be9c1088571d9720843d605337a5ed15200
yisy0703 <yisy7003@naver.com>
2024-08-27 12:53:18 +0900

    Java operation

---
+++ source/01_java/ch03_operator/src/com/lec/ex/Ex06.java
@@ -0,0 +1,9 @@
+package com.lec.ex;
+// 할당(대입)연산자 : =, +=, -=, *=, ... 
+public class Ex06 {
+	public static void main(String[] args) {
+		int n1 = 10;
+		n1 += 2; // n1 = n1 + 2;
+		System.out.println("n1 = " + n1);
+	}
+}
