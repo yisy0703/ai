69f6bfaad423540dfbe41bf7bfed95e619bcf9bc
yisy0703 <yisy7003@naver.com>
2024-08-27 17:13:15 +0900

    Java if

--- source/01_java/ch04_control/src/com/lec/condition/Ex03_switch.java
+++ source/01_java/ch04_control/src/com/lec/condition/Ex03_switch.java
@@ -2,18 +2,19 @@ package com.lec.condition;
 public class Ex03_switch {
 	public static void main(String[] args) {
 		int num=3;
-		if(num==1) {
-			System.out.println("주사위 1");
-		}else if(num==2) {
-			System.out.println("주사위 2");
-		}else if(num==3) {
-			System.out.println("주사위 3");
-		}else if(num==4) {
-			System.out.println("주사위 4");
-		}else if(num==5) {
-			System.out.println("주사위 5");
-		}else {
-			System.out.println("주사위 6");
-		}
+		
+//		if(num==1) {
+//			System.out.println("주사위 1");
+//		}else if(num==2) {
+//			System.out.println("주사위 2");
+//		}else if(num==3) {
+//			System.out.println("주사위 3");
+//		}else if(num==4) {
+//			System.out.println("주사위 4");
+//		}else if(num==5) {
+//			System.out.println("주사위 5");
+//		}else {
+//			System.out.println("주사위 6");
+//		}
 	}
 }
