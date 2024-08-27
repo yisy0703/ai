118ed5ef9b3f9362dc0337bf4d10a392cb07d8c5
yisy0703 <yisy7003@naver.com>
2024-08-27 12:34:15 +0900

    Java operation

--- source/01_java/ch03_operator/src/com/lec/ex/Ex04.java
+++ source/01_java/ch03_operator/src/com/lec/ex/Ex04.java
@@ -15,6 +15,9 @@ public class Ex04 {
 		int i=1, j=10, h=10;
 		System.out.println("( (i<j) && (++j>h) ) =>" + ( (i<j) && (++j>h) ) );
 		System.out.println("j = " + j);
+		
+		System.out.println("( (i<j) && (++j>h) ) =>" + ( (i<j) && (++j>h) ) );
+		System.out.println("j = " + j);
 	}
 }
 
