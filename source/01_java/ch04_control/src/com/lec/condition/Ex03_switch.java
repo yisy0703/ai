17982ae37f342891ba8aa3c891d3ea06dec9ca47
yisy0703 <yisy7003@naver.com>
2024-08-27 16:59:19 +0900

    Java if

--- source/01_java/ch04_control/src/com/lec/condition/Ex03_switch.java
+++ source/01_java/ch04_control/src/com/lec/condition/Ex03_switch.java
@@ -4,6 +4,16 @@ public class Ex03_switch {
 		int num=3;
 		if(num==1) {
 			System.out.println("주사위 1");
+		}else if(num==2) {
+			System.out.println("주사위 2");
+		}else if(num==3) {
+			System.out.println("주사위 3");
+		}else if(num==4) {
+			System.out.println("주사위 4");
+		}else if(num==5) {
+			System.out.println("주사위 5");
+		}else {
+			System.out.println("주사위 6");
 		}
 	}
 }
