954e505c7900d0f7fc08e065a82b4550fd123c0e
yisy0703 <yisy7003@naver.com>
2024-08-27 12:05:14 +0900

    Java operation

---
+++ source/01_java/ch03_operator/src/com/lec/ex/Ex03.java
@@ -0,0 +1,9 @@
+package com.lec.ex;
+// 관계연산자(비교연산자) : ==(같다) !=(다르다), >, >=, <, <= / 관계연산의 결과는 boolean
+public class Ex03 {
+	public static void main(String[] args) {
+		int n1=10, n2=5;
+		boolean result;
+		
+	}
+}
