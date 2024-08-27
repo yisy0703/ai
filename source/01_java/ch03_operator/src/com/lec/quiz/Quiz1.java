7eb0d512e242a6f2ae8eae904e292a67bc9dbd4a
yisy0703 <yisy7003@naver.com>
2024-08-27 15:30:30 +0900

    Java operation

--- source/01_java/ch03_operator/src/com/lec/quiz/Quiz1.java
+++ source/01_java/ch03_operator/src/com/lec/quiz/Quiz1.java
@@ -1,5 +1,14 @@
 package com.lec.quiz;
 
-public class Quiz1 {
+import java.util.Scanner;
 
+public class Quiz1 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("수 입력 : ");
+		int i = scanner.nextInt();
+		int result = i%2;
+		System.out.println((result==0)? "입력한 수는 짝수":"입력한 수는 홀수");
+		scanner.close();
+	}
 }
--- source/01_java/ch03_operator/src/com/lec/quiz/Quiz2.java
+++ source/01_java/ch03_operator/src/com/lec/quiz/Quiz2.java
@@ -1,5 +1,20 @@
 package com.lec.quiz;
 
-public class Quiz2 {
+import java.util.Scanner;
 
+public class Quiz2 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("국어 점수는?");
+		int kor = scanner.nextInt();
+		System.out.print("영어 점수는?");
+		int eng = scanner.nextInt();
+		System.out.print("수학 점수는?");
+		int mat = scanner.nextInt();
+		int sum = kor + eng + mat;
+		double avg = sum/3.;
+		System.out.printf("국어 = %d, 영어 = %d, 수학 = %d\n",kor, eng, mat);
+		System.out.printf("총점 = %d, 평균 = %.2f\n", sum, avg);
+		scanner.close();
+	}
 }
