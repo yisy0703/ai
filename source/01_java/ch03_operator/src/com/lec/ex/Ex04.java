b77c74bfae2da476981d33ef1ffa720de03a6f5c
yisy0703 <yisy7003@naver.com>
2024-08-27 12:37:45 +0900

    Java operation

--- source/01_java/ch03_operator/src/com/lec/ex/Ex04.java
+++ source/01_java/ch03_operator/src/com/lec/ex/Ex04.java
@@ -18,6 +18,12 @@ public class Ex04 {
 		// &&(and)연산의 좌항이 false인 경우, 우항을 보지도 않고 결과를 false로
 		System.out.println("( (i>j) && (++j>h) ) =>" + ( (i>j) && (++j>h) ) );
 		System.out.println("j = " + j);
+		// ||(or)
+		System.out.println("( (i<j) || (++j>h) ) =>" + ( (i<j) || (++j>h) ) );
+		System.out.println("j = " + j);
+		// ||(or)연산의 좌항이 true인 경우, 우항을 보지도 않고 결과를 true로
+		System.out.println("( (i>j) || (++j>h) ) =>" + ( (i>j) || (++j>h) ) );
+		System.out.println("j = " + j);
 	}
 }
 
