2f33c89d6a2b468a16b496177e6ed11e7dd550dc
yisy0703 <yisy7003@naver.com>
2024-08-28 10:36:20 +0900

    Java if

--- source/01_java/ch04_control/src/com/lec/condition/Ex06_switch_randomHakjum.java
+++ source/01_java/ch04_control/src/com/lec/condition/Ex06_switch_randomHakjum.java
@@ -3,11 +3,13 @@ package com.lec.condition;
 public class Ex06_switch_randomHakjum {
 	public static void main(String[] args) {
 		// 0 <= Math.random() < 1 : 실수 난수
+		// 0 <= Math.random()*51 < 51 : 실수 난수
+		// 50<= Math.random()*51 + 50 < 101 : 실수
+		// 50<= (int)(Math.random()*51 + 50) < 101 : 정수 (50~100)		
 		// 0 <= Math.random()*101 < 101 : 실수 난수
-		// 0 <= (int)(Math.random()*101) <101 : 정수 난수
+		// 0 <= (int)(Math.random()*101) <101 : 정수 난수(0~100)
 		// 0 <= Math.random()*45 < 45 : 실수
 		// 1 <= (int)(Math.random()*45+1) < 46 : 정수
-		// 50<= score < 101 : 정수 
 		System.out.println( (int)(Math.random()*101) );
 	}
 }
