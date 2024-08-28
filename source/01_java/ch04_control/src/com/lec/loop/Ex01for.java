d604ec444229a3dc8f791c44bd586f99fa5eae0e
yisy0703 <yisy7003@naver.com>
2024-08-28 15:51:11 +0900

    Java loop

---
+++ source/01_java/ch04_control/src/com/lec/loop/Ex01for.java
@@ -0,0 +1,11 @@
+package com.lec.loop;
+// for - 정해진 횟수만큼 반복
+public class Ex01for {
+	public static void main(String[] args) {
+		
+		for(int i=0 ; i<5 ; i++) {
+			System.out.println(i + ", 안녕하세요");
+		}
+		// System.out.println(i);
+	}
+}
