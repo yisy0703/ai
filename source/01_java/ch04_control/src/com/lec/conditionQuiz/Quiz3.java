04dbda00d67cee5e7a9987c5b4196a7cd3fa6fd1
yisy0703 <yisy7003@naver.com>
2024-08-28 13:07:34 +0900

    Java if

---
+++ source/01_java/ch04_control/src/com/lec/conditionQuiz/Quiz3.java
@@ -0,0 +1,32 @@
+package com.lec.conditionQuiz;
+import java.util.Scanner;
+public class Quiz3 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("가위(0), 바위(1), 보(2)중 하나를 선택하세요 :");
+		int you = scanner.nextInt(); // 0, 1, 2
+		String youStr;
+//		if (you == 0) {
+//			youStr = "가위";
+//		} else if (you == 1) {
+//			youStr = "바위";
+//		} else if (you == 2) {
+//			youStr = "보";
+//		} else {
+//			youStr = "유효하지 않은 값";
+//		}
+//		switch (you) {
+//		case 0:
+//			youStr = "가위"; break;
+//		case 1:
+//			youStr = "바위"; break;
+//		case 2: 
+//			youStr = "보"; break;
+//		default:
+//			youStr = "유효하지 않은 값"
+//		}
+		youStr = (you==0)? "가위": (you==1)? "바위": (you==2)?"보":"유효하지 않는 값";
+		System.out.printf("당신은 %s를 내셨습니다", youStr);
+		scanner.close();
+	}
+}
--- source/01_java/ch04_control/src/com/lec/conditionQuiz/Quiz4_srp_com.java
+++ source/01_java/ch04_control/src/com/lec/conditionQuiz/Quiz4_srp_com.java
@@ -1,5 +1,7 @@
 package com.lec.conditionQuiz;
+
 import java.util.Scanner;
+
 // 사용자에게 가위(0),바위(1),보(2) 입력받는다 
 //    1. 0~2사이의 수를 입력 안 한 경우 : 프로그램 강제 종료
 //    2. 0~2사이의 수를 입력 한 경우 (if, switch, 삼항연산자)
@@ -11,11 +13,11 @@ public class Quiz4_srp_com {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("가위(0),바위(1),보(2) 입력 : ");
 		int you = sc.nextInt();
-		if( you<0 || you>2 ) {
+		if (you < 0 || you > 2) {
 			System.out.println("떼끼");
 			System.exit(0); // 프로그램 정상 강제 종료
 		}
-		String youStr = (you==0) ?  "가위"  :  (you==1) ? "바위":"보"  ;
+		String youStr = (you == 0) ? "가위" : (you == 1) ? "바위" : "보";
 //		if(you==0) {
 //			youStr = "가위";
 //		}else if(you==1){
@@ -29,27 +31,31 @@ public class Quiz4_srp_com {
 //		default : youStr="보"; break;
 //		}		
 		// 컴퓨터도 0~2 난수 0 <= Math.random()*3 < 3
-		int com = 1; // 0~2사이의 정수 난수;
-		String comStr = (com==0) ? "가위" : (com==1) ? "바위" : "보";
+		int com = (int) (Math.random() * 3);
+		String comStr = (com == 0) ? "가위" : (com == 1) ? "바위" : "보";
 		System.out.println("당신은 " + youStr + ", 컴퓨터는 " + comStr);
 		// if문이나 switch문 이용해서 가위바위보 결과 출력
+		// 당신이 이겼습니다 || 컴퓨터가 이겼습니다 || 비겼습니다 출력
+		if (you == com) {
+			System.out.println("비겼습니다 -.-");
+		} else if (you == 0) {
+			if (com == 1) {
+				System.out.println("컴퓨터가 이겼습니다 ㅠㅠ");
+			} else {
+				System.out.println("★ 당신이 이겼습니다 ★");
+			}
+		} else if (you == 1) {
+			if (com == 0) {
+				System.out.println("★ 당신이 이겼습니다 ★");
+			} else {
+				System.out.println("컴퓨터가 이겼습니다 ㅠㅠ");
+			}
+		} else if (you == 2) {
+			if (com == 0) {
+				System.out.println("컴퓨터가 이겼습니다 ㅠㅠ");
+			} else {
+				System.out.println("★ 당신이 이겼습니다 ★");
+			}
+		} // 결과
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
