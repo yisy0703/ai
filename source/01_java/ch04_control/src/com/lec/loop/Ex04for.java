1dd5b9367e0e0fd809626902514240804c61ba18
yisy0703 <yisy7003@naver.com>
2024-08-28 16:40:42 +0900

    Java loop

---
+++ source/01_java/ch04_control/src/com/lec/loop/Ex04for.java
@@ -0,0 +1,16 @@
+package com.lec.loop;
+/* * * * *
+ * * * * *
+ * * * * *
+ * * * * *
+ * * * * */
+public class Ex04for {
+	public static void main(String[] args) {
+		for(int i=1 ; i<=5 ; i++) {
+			for(int j=1 ; j<=5 ; j++) {
+				System.out.print('*');
+			} // for-j
+			System.out.println(); // 개행만
+		} // for-i
+	}// main
+} // class
