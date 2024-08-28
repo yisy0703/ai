fb83f18910f33e7b5aba27133d3021f0b2247849
yisy0703 <yisy7003@naver.com>
2024-08-28 10:20:52 +0900

    Java if

--- source/01_java/ch04_control/src/com/lec/condition/Ex06_switch_randomHakjum.java
+++ source/01_java/ch04_control/src/com/lec/condition/Ex06_switch_randomHakjum.java
@@ -1,5 +1,13 @@
 package com.lec.condition;
-
+// 실행할 때마나 컴퓨터가 난수(random) 점수를 발생하여 학점 출력
 public class Ex06_switch_randomHakjum {
-
+	public static void main(String[] args) {
+		// 0 <= Math.random() < 1 : 실수 난수
+		// 0 <= Math.random()*101 < 101 : 실수 난수
+		// 0 <= (int)(Math.random()*101) <101 : 정수 난수
+		// 0 <= Math.random()*45 < 45 : 실수
+		// 1 <= (int)(Math.random()*45+1) < 46 : 정수
+		// 50<= score < 101 : 정수 
+		System.out.println( (int)(Math.random()*101) );
+	}
 }
