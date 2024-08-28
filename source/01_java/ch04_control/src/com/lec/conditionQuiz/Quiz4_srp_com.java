5896db29e3e11a6836f96a8d07fcaa0351a1e5c2
yisy0703 <yisy7003@naver.com>
2024-08-28 12:21:44 +0900

    Java if

--- source/01_java/ch04_control/src/com/lec/conditionQuiz/Quiz4_srp_com.java
+++ source/01_java/ch04_control/src/com/lec/conditionQuiz/Quiz4_srp_com.java
@@ -16,11 +16,11 @@ public class Quiz4_srp_com {
 			System.exit(0); // 프로그램 정상 강제 종료
 		}
 		String youStr; // 가위나 바위나 보를 저장
-		switch(you) {
-		case 0 : youStr="가위"; break;
-		case 1 : youStr="바위"; break;
-		default : youStr="보"; break;
-		}
+//		switch(you) {
+//		case 0 : youStr="가위"; break;
+//		case 1 : youStr="바위"; break;
+//		default : youStr="보"; break;
+//		}
 //		if(you==0) {
 //			youStr = "가위";
 //		}else if(you==1){
