90ff612d4daee879f4e1b22b2dcd3bbedeb5565a
yisy0703 <yisy7003@naver.com>
2024-08-28 16:19:10 +0900

    Java loop

--- source/01_java/ch04_control/src/com/lec/loop/Ex02for.java
+++ source/01_java/ch04_control/src/com/lec/loop/Ex02for.java
@@ -3,14 +3,15 @@ package com.lec.loop;
 // 1+2+3+4+5+6+7+8+9+10 = 55
 public class Ex02for {
 	public static void main(String[] args) {
-		int total = 0;
-		
+		int total = 0;		
 		for( int i=1 ; i<=10 ; i++ ) {
-			
 			total += i; //total = total + i;
-			System.out.print(i + "+");
-		}
-		
+			if(i==10) {
+				System.out.print(i + '=' );
+			}else {
+				System.out.print(i + "+");
+			}
+		}		
 		System.out.println(total);
 	}
 }
