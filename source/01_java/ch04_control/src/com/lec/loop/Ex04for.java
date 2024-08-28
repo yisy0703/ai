0b778c27c3e1e0a4650578d7057a3d8eb33a8bc7
yisy0703 <yisy7003@naver.com>
2024-08-28 17:02:20 +0900

    Java loop

--- source/01_java/ch04_control/src/com/lec/loop/Ex04for.java
+++ source/01_java/ch04_control/src/com/lec/loop/Ex04for.java
@@ -1,13 +1,13 @@
 package com.lec.loop;
-/* * * * *
- * * * * *
- * * * * *
- * * * * *
+/* 
+ * * 
+ * * * 
+ * * * * 
  * * * * */
 public class Ex04for {
 	public static void main(String[] args) {
 		for(int i=1 ; i<=5 ; i++) {
-			for(int j=1 ; j<=5 ; j++) {
+			for(int j=1 ; j<=i ; j++) {
 				System.out.print('*');
 			} // for-j
 			System.out.println(); // 개행만
---
+++ source/01_java/ch04_control/src/com/lec/loopQuiz/Quiz4_gugudan.java
@@ -0,0 +1,14 @@
+package com.lec.loopQuiz;
+// 2*1= 2 3*1= 3 ....  9*1= 9
+// ...
+// 2*9=18 3*9=27 ..    9*9=81
+public class Quiz4_gugudan {
+	public static void main(String[] args) {
+		for(int j=1 ; j<=9 ; j++) {
+			for(int i=2 ; i<=9 ; i++) {
+				System.out.printf("%d * %d = %2d\t", i, 9, i*9);
+			}
+			System.out.println(); // 개행만
+		}
+	}
+}
