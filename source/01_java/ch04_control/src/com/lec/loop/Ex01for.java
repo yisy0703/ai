92b3c6c85eee2ae05ca5f6c725afe4e31213a965
yisy0703 <yisy7003@naver.com>
2024-08-28 16:17:30 +0900

    Java loop

--- source/01_java/ch04_control/src/com/lec/loop/Ex01for.java
+++ source/01_java/ch04_control/src/com/lec/loop/Ex01for.java
@@ -4,7 +4,7 @@ public class Ex01for {
 	public static void main(String[] args) {
 		
 		for(int i=0 ; i<5 ; i++) {
-			System.out.println(i + ", 안녕하세요");
+			System.out.println("안녕하세요");
 		}
 		// System.out.println(i);
 	}
