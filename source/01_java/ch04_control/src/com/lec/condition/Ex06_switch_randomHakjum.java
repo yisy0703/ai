4f804ab23c071b1e4cf8e3a08dc186fa5cc74beb
yisy0703 <yisy7003@naver.com>
2024-08-28 10:51:41 +0900

    Java if

--- source/01_java/ch04_control/src/com/lec/condition/Ex06_switch_randomHakjum.java
+++ source/01_java/ch04_control/src/com/lec/condition/Ex06_switch_randomHakjum.java
@@ -10,6 +10,35 @@ public class Ex06_switch_randomHakjum {
 		// 0 <= (int)(Math.random()*101) <101 : 정수 난수(0~100)
 		// 0 <= Math.random()*45 < 45 : 실수
 		// 1 <= (int)(Math.random()*45+1) < 46 : 정수
-		System.out.println( (int)(Math.random()*101) );
+		int score = (int)(Math.random()*101);
+		System.out.print( "점수는 " + score + " - ");
+		switch(score/10) {
+		case 10:
+		case 9:
+			System.out.println("A학점"); break; // 제어문 탈출
+		case 8:
+			System.out.println("B학점"); break;
+		case 7:
+			System.out.println("C학점"); break;
+		case 6:
+			System.out.println("D학점"); break;
+		default:
+			System.out.println("F학점");
+		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ source/01_java/ch04_control/src/com/lec/condition/Ex07.java
@@ -0,0 +1,7 @@
+package com.lec.condition;
+// 입력받은 수가 홀수인지 짝수인지 출력(if, switch, 삼항연산자)
+public class Ex07 {
+	public static void main(String[] args) {
+		
+	}
+}
