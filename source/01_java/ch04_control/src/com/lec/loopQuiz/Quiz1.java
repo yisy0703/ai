de4334947be0a0b340c983ffac4fdd2fc3be017f
yisy0703 <yisy7003@naver.com>
2024-08-28 17:55:51 +0900

    Java loop

--- source/01_java/ch04_control/src/com/lec/loopQuiz/Quiz1.java
+++ source/01_java/ch04_control/src/com/lec/loopQuiz/Quiz1.java
@@ -1,5 +1,5 @@
 package com.lec.loopQuiz;
-//1~10까지의 곱을 구해보자
+//퀴즈 ① 1~10까지의 곱의 결과를 출력해 보자(for문이용)
 public class Quiz1 {
 	public static void main(String[] args) {
 		int result = 1;
--- source/01_java/ch04_control/src/com/lec/loopQuiz/Quiz2.java
+++ source/01_java/ch04_control/src/com/lec/loopQuiz/Quiz2.java
@@ -1,5 +1,5 @@
 package com.lec.loopQuiz;
-//1~10까지 숫자 중 짝수의 합과 홀수의 합을 구해보자
+//퀴즈 ② 1~10까지 숫자 중 홀수의 합을 구해보자(for문이용)
 public class Quiz2 {
 	public static void main(String[] args) {
 		int oddTotal = 0; // 홀수 누적 변수
