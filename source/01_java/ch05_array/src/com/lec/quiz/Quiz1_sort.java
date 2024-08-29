9dd79025aa0db79f8011c7210a4377d4b6c30c50
yisy0703 <yisy7003@naver.com>
2024-08-29 17:16:34 +0900

    Java array

--- source/01_java/ch05_array/src/com/lec/quiz/Quiz1_sort.java
+++ source/01_java/ch05_array/src/com/lec/quiz/Quiz1_sort.java
@@ -8,5 +8,26 @@ public class Quiz1_sort {
 //			System.out.print(a + "\t");
 //		}
 		System.out.println("정렬전 : " + Arrays.toString(arr));
+		// 정렬
+		for(int i=0 ; i<arr.length-1 ; i++) {
+			for(int j=i+1 ; j<arr.length ; j++) {
+				if(arr[i] > arr[j]) { 
+					// i번째와 j번째 바꿔
+				}
+			}
+		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
