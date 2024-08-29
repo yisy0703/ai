0b99cb4aaf6c31f9f399648a2061803e0fc9c4d9
yisy0703 <yisy7003@naver.com>
2024-08-29 15:24:51 +0900

    Java loop

---
+++ source/01_java/ch05_array/src/com/lec/ex/Ex04_arrayCopy.java
@@ -0,0 +1,18 @@
+package com.lec.ex;
+
+public class Ex04_arrayCopy {
+	public static void main(String[] args) {
+		int[] score = {100, 20, 30, 40, 50};
+		int[] s = new int[score.length];
+//		for(int idx=0 ; idx<s.length ; idx++) {
+//			s[idx] = score[idx];
+//		}
+		for(int idx=0 ; idx<score.length ; idx++) {
+			System.out.printf("score[%d]=%d \t s[%d]=%d\n", idx, score[idx], idx, s[idx]);
+		}
+		s[0] = 99; // s[0]만 변경
+		for(int idx=0 ; idx<score.length ; idx++) {
+			System.out.printf("score[%d]=%d \t s[%d]=%d\n", idx, score[idx], idx, s[idx]);
+		}
+	}
+}
