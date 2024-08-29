630cd4098ce96128e06b4293ada367b7fce62c0e
yisy0703 <yisy7003@naver.com>
2024-08-29 16:43:08 +0900

    Java array

--- source/01_java/ch05_array/src/com/lec/ex/Ex05height.java
+++ source/01_java/ch05_array/src/com/lec/ex/Ex05height.java
@@ -13,9 +13,36 @@ public class Ex05height {
 			heights[idx] = scanner.nextInt();
 			totHeight += heights[idx]; // 키 누적
 		}//for
-		
+		// 친구들 키 출력
+		for(int idx=0 ; idx<names.length ; idx++) {
+			System.out.printf("%d번째 %s의 키는 %d\n", idx, names[idx], heights[idx]);
+		}
+		System.out.println("평균키 : " + (double)totHeight/names.length);
+		// 최장신키, 최장신이름, 최단신키, 최단신이름
+		int maxHeight=heights[0], maxIdx = 0 ; // max값 초기화는 아주 작은값이나 0방
+		int minHeight=heights[0], minIdx = 0 ; // min값 초기화는 아주 큰값이나 0방
+		for(int idx=0 ; idx<names.length ; idx++) {
+			if(heights[idx] > maxHeight ) { // 최대값
+				maxHeight = heights[idx]; 
+				maxIdx = idx;
+			} // if 최대값
+			if(heights[idx] < minHeight) { // 최소값
+				minHeight = heights[idx];
+				minIdx = idx;
+			} // if 최소값
+		} // for
+		System.out.printf("최장신 %s의 키는 %d\n", names[maxIdx], maxHeight);
+		System.out.printf("최단신 %s의 키는 %d\n", names[minIdx], minHeight);
 	}
 }
+
+
+
+
+
+
+
+
 
 
 
