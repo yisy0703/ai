4899fc73cdd71e0f042182eba8834e035e3729d7
yisy0703 <yisy7003@naver.com>
2024-08-29 15:26:52 +0900

    Java loop

--- source/01_java/ch05_array/src/com/lec/ex/Ex04_arrayCopy.java
+++ source/01_java/ch05_array/src/com/lec/ex/Ex04_arrayCopy.java
@@ -4,6 +4,7 @@ public class Ex04_arrayCopy {
 	public static void main(String[] args) {
 		int[] score = {100, 20, 30, 40, 50};
 		int[] s = new int[score.length];
+		System.arraycopy(score, 0, s, 0, score.length);
 //		for(int idx=0 ; idx<s.length ; idx++) {
 //			s[idx] = score[idx];
 //		}
