d412bfe2cd6dc7031abadc6d2e218ea1eddddaf7
yisy0703 <yisy7003@naver.com>
2024-08-29 16:22:18 +0900

    Java array

--- source/01_java/ch05_array/src/com/lec/ex/Ex05height.java
+++ source/01_java/ch05_array/src/com/lec/ex/Ex05height.java
@@ -1,5 +1,27 @@
 package com.lec.ex;
+import java.util.Scanner;
 // 길동, 현식, 땅땅, 슘당, 대갈 친구의 키를 입력받고, 평균, 최장신 친구의 이름과 키, 최단신 친구의 이름과 키
 public class Ex05height {
-
+	public static void main(String[] args) {
+		String[] names = {"길동", "현식", "땅땅", "슘당", "대갈"};
+		int[] heights = new int[names.length];
+		Scanner scanner = new Scanner(System.in);
+		int totHeight = 0; // 키 누적변수
+		// 친구들 키 입력받아 heights배열에 할당, 키 누적
+		for(int idx=0 ; idx<names.length ; idx++) {
+			System.out.print(names[idx] + "의 키는 ? ");
+			heights[idx] = scanner.nextInt();
+			totHeight += heights[idx]; // 키 누적
+		}//for
+		
+	}
 }
+
+
+
+
+
+
+
+
+
