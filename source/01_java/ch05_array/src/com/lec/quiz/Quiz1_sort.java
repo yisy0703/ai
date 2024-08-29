c29c77c62ef542574c4564de7450c737181cbb88
yisy0703 <yisy7003@naver.com>
2024-08-29 17:02:09 +0900

    Java array

---
+++ source/01_java/ch05_array/src/com/lec/quiz/Quiz1_sort.java
@@ -0,0 +1,12 @@
+package com.lec.quiz;
+import java.util.Arrays;
+// 배열의 오름차순 정렬
+public class Quiz1_sort {
+	public static void main(String[] args) {
+		int[] arr = {76,45,34,89,50};
+//		for(int a : arr) {
+//			System.out.print(a + "\t");
+//		}
+		System.out.println("정렬전 : " + Arrays.toString(arr));
+	}
+}
