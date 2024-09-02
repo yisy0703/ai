d028ca8cf80bfe521139a40272484a37f9290e47
yisy0703 <yisy7003@naver.com>
2024-09-02 16:27:19 +0900

    Java oop

--- source/01_java/ch07_class/src/com/lec/ex2_rect/Rect.java
+++ source/01_java/ch07_class/src/com/lec/ex2_rect/Rect.java
@@ -1,7 +1,38 @@
 package com.lec.ex2_rect;
 // 클래스 : 속성(인스턴스변수), 생성자함수(속성데이터초기화), 메소드, setter&getter
-// Rect r1 = new Rect()
+// Rect r1 = new Rect(); r1.setWidth(2); r1.setHeight(4);
 // Rect r1 = new Rect(2, 4)
+// Rect r1 = new Rect(3);
 public class Rect {
-
+	private int width;
+	private int height;
+	// 생성자 함수가 없을 경우, JVM이 디폴트 생성자 자동 생성
+	public Rect() {} // 디폴트 생성자
+	public Rect(int side) {
+		width = side;
+		height = side;
+	}
+	public Rect(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
+	public int area() {
+		return width*height;
+	}
+	// setter & getter
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
