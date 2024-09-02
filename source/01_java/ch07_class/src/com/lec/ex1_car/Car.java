0b50efa81f395a2017b6727fff303d11b136aecd
yisy0703 <yisy7003@naver.com>
2024-09-02 16:12:49 +0900

    Java oop

--- source/01_java/ch07_class/src/com/lec/ex1_car/Car.java
+++ source/01_java/ch07_class/src/com/lec/ex1_car/Car.java
@@ -6,6 +6,8 @@ public class Car {
 	private String color; 
 	private int    cc; // 배기량 1500 2000
 	private int    speed;
+	// 생성자 함수 없으면 디폴트 생성자 함수를 JVM 자동 생성
+	public Car() { }
 	public void park() {
 		speed = 0;
 		System.out.println(color + "색 주차함. 지금 속도 : " + speed);
---
+++ source/01_java/ch07_class/src/com/lec/ex2_rect/Rect.java
@@ -0,0 +1,7 @@
+package com.lec.ex2_rect;
+// 클래스 : 속성(인스턴스변수), 생성자함수(속성데이터초기화), 메소드, setter&getter
+// Rect r1 = new Rect()
+// Rect r1 = new Rect(2, 4)
+public class Rect {
+
+}
