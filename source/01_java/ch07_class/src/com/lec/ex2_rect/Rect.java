01d1fe4968365408eebf1b73ddb9a95dda9d212a
yisy0703 <yisy7003@naver.com>
2024-09-02 16:44:28 +0900

    Java oop

--- source/01_java/ch07_class/src/com/lec/ex2_rect/Rect.java
+++ source/01_java/ch07_class/src/com/lec/ex2_rect/Rect.java
@@ -20,7 +20,18 @@ public class Rect {
 		return width*height;
 	}
 	// setter & getter
-	
+	public int getWidth() {
+		return width;
+	}
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
 }
 
 
---
+++ source/01_java/ch07_class/src/com/lec/ex2_rect/RectMain.java
@@ -0,0 +1,18 @@
+package com.lec.ex2_rect;
+
+public class RectMain {
+	public static void main(String[] args) {
+		Rect r1 = new Rect();
+		Rect r2 = new Rect(3);
+		Rect r3 = new Rect(2, 4);
+		// 가로 세로 길이 출력
+		System.out.printf("r1의 데이터 : %d, %d\n", r1.getWidth(), r1.getHeight());
+		System.out.printf("r2의 데이터 : %d, %d\n", r2.getWidth(), r2.getHeight());
+		System.out.printf("r3의 데이터 : %d, %d\n", r3.getWidth(), r3.getHeight());
+		r1.setWidth(10); r1.setHeight(5);
+		System.out.println("r1변경후");
+		System.out.printf("r1의 데이터 : %d, %d\n", r1.getWidth(), r1.getHeight());
+		System.out.printf("r2의 데이터 : %d, %d\n", r2.getWidth(), r2.getHeight());
+		System.out.printf("r3의 데이터 : %d, %d\n", r3.getWidth(), r3.getHeight());
+	}
+}
