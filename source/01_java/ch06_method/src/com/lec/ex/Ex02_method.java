486c91f7528820ab83bcdf612418a18c1b2f87b4
yisy0703 <yisy7003@naver.com>
2024-09-02 10:26:30 +0900

    Java oop

--- source/01_java/ch06_method/src/com/lec/ex/Ex02_method.java
+++ source/01_java/ch06_method/src/com/lec/ex/Ex02_method.java
@@ -1,25 +1,20 @@
 package com.lec.ex;
 // x부터 y까지 정수 합과 그 결과가 짝/홀수인지 출력
-public class Ex01_noMethod {
+public class Ex02_method {
+	private static int sum(int from, int to) {
+		return 100;
+	}
+	
 	public static void main(String[] args) {
-		int tot = 0; // 누적변수
-		for(int i=1 ; i<=10 ; i++) {
-			tot += i; //tot = tot + i;
-		}
+		int tot = sum(1, 10); 
 		System.out.println("1부터 10까지 정수의 합은 " + tot);
 		System.out.println(tot%2==0 ? "짝수입니다":"홀수입니다");
 		//------ 주 ---------- 석 -------------------
-		tot = 0; // 누적변수
-		for(int i=10 ; i<=100 ; i++) {
-			tot += i; //tot = tot + i;
-		}
+		tot = sum(10, 100);
 		System.out.println("10부터 100까지 정수의 합은 " + tot);
 		System.out.println(tot%2==0 ? "짝수입니다":"홀수입니다");
 		//------ 주 ---------- 석 -------------------
-		tot = 0; // 누적변수
-		for(int i=5 ; i<=10 ; i++) {
-			tot += i; //tot = tot + i;
-		}
+		tot = sum(5, 10);
 		System.out.println("5부터 10까지 정수의 합은 " + tot);
 	}
 }
