be9b76bf744d7f14d068320ac1aa5bc853a943b5
yisy0703 <yisy7003@naver.com>
2024-09-02 12:15:02 +0900

    Java oop

--- source/01_java/ch06_method/src/com/lec/ex/Ex04_void.java
+++ source/01_java/ch06_method/src/com/lec/ex/Ex04_void.java
@@ -4,11 +4,23 @@ public class Ex04_void {
 	public static void main(String[] args) {
 		printLine(30);
 		System.out.println("Hello, World!");
-		printLine();
+		printLine("=");
 		System.out.println("Hello, Java");
-		printLine();
+		printLine(30, "~");
 		System.out.println("Hello, World!");
 		printLine();
+	}
+	private static void printLine(int cnt, String ch) {
+		for(int i=0 ; i<cnt ; i++) {
+			System.out.print(ch);
+		}
+		System.out.println(); // 개행
+	}
+	private static void printLine(String ch) {
+		for(int i=0 ; i<20 ; i++) {
+			System.out.print(ch);
+		}
+		System.out.println(); // 개행
 	}
 	private static void printLine(int cnt) {
 		for(int i=0 ; i<cnt ; i++) {
