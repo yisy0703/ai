7717d4560bb1d606fb6889fcb5e736a8d93d9b04
yisy0703 <yisy7003@naver.com>
2024-09-02 12:39:28 +0900

    Java oop

---
+++ source/01_java/ch06_method/src/com/lec/quiz/Quiz_gugudan.java
@@ -0,0 +1,12 @@
+package com.lec.quiz;
+import java.util.Scanner;
+// 사용자로부터 구구단 수를 입력받아 gugudanOut(입력받은 수) 호출 -> 호출한 메소드에서 대항 구구단 출력
+public class Quiz_gugudan {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("출력하고자 하는 구구단 수는 > ");
+		int dansu = scanner.nextInt();
+		System.out.println("입력하신 구구단 수는 " + dansu);
+		gugudanOut(dansu);
+	}
+}
