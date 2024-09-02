9ef50c89155750600ae418b5c1c00ca3c55628b3
yisy0703 <yisy7003@naver.com>
2024-09-02 10:55:50 +0900

    Java oop

--- source/01_java/ch06_method/src/com/lec/ex/Ex02_method.java
+++ source/01_java/ch06_method/src/com/lec/ex/Ex02_method.java
@@ -1,10 +1,23 @@
 package com.lec.ex;
+
 // x부터 y까지 정수 합과 그 결과가 짝/홀수인지 출력
 public class Ex02_method {
-	private static int sum(int from, int to) {
-		return 100;
+	private static int sum(int from, int to) { // from부터 to까지 누적합 return
+		int sum = 0; // 누적변수
+		for(int i=from ; i<=to ; i++) {
+			sum += i; // sum = sum+i;
+		}
+		return sum;
 	}
-	
+	private static String evenOdd(int value) { // value가 짝수인지 홀수인지를 return
+		String result = "짝수입니다";
+		return result;
+//		if (value%2==0) {
+//			return "짝수입니다";
+//		}else {
+//			return "홀수입니다";
+//		}
+	}
 	public static void main(String[] args) {
 		int tot = sum(1, 10); 
 		System.out.println("1부터 10까지 정수의 합은 " + tot);
@@ -18,3 +31,7 @@ public class Ex02_method {
 		System.out.println("5부터 10까지 정수의 합은 " + tot);
 	}
 }
+
+
+
+
