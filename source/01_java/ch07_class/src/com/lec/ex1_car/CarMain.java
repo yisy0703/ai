a5354676f8a64f52e434896a2431b45864626efc
yisy0703 <yisy7003@naver.com>
2024-09-02 15:02:50 +0900

    Java oop

---
+++ source/01_java/ch07_class/src/com/lec/ex1_car/CarMain.java
@@ -0,0 +1,31 @@
+package com.lec.ex1_car;
+
+public class CarMain {
+	public static void main(String[] args) {
+		Car myPorsche = new Car();
+		myPorsche.color="빨강";
+		myPorsche.cc = 2400;
+		System.out.println(myPorsche.color + "색차량 배기량은 " + 
+				myPorsche.cc + ", 속도는 " + myPorsche.speed);
+		myPorsche.drive();
+		myPorsche.park();
+		myPorsche.race();
+		System.out.println(myPorsche.color + "색차량 배기량은 " + 
+				myPorsche.cc + ", 속도는 " + myPorsche.speed);
+		Car yourPorsche = new Car();
+		yourPorsche.color = "gray";
+		yourPorsche.drive();
+		System.out.println(myPorsche.color + "색차량 배기량은 " + 
+				myPorsche.cc + ", 속도는 " + myPorsche.speed);
+		System.out.println(yourPorsche.color + "색차량 배기량은 " + 
+				yourPorsche.cc + ", 속도는 " + yourPorsche.speed);
+	}
+}
+
+
+
+
+
+
+
+
