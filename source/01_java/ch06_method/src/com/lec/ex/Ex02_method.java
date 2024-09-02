5eb78f8fbbcd32e711383b2fdc4a0f2eced4b78d
yisy0703 <yisy7003@naver.com>
2024-09-02 11:18:05 +0900

    Java oop

--- source/01_java/ch06_method/src/com/lec/ex/Ex02_method.java
+++ source/01_java/ch06_method/src/com/lec/ex/Ex02_method.java
@@ -21,11 +21,11 @@ public class Ex02_method {
 	public static void main(String[] args) {
 		int tot = sum(1, 10); 
 		System.out.println("1부터 10까지 정수의 합은 " + tot);
-		System.out.println(tot%2==0 ? "짝수입니다":"홀수입니다");
+		System.out.println(evenOdd(tot));
 		//------ 주 ---------- 석 -------------------
 		tot = sum(10, 100);
 		System.out.println("10부터 100까지 정수의 합은 " + tot);
-		System.out.println(tot%2==0 ? "짝수입니다":"홀수입니다");
+		System.out.println(evenOdd(tot));
 		//------ 주 ---------- 석 -------------------
 		tot = sum(5, 10);
 		System.out.println("5부터 10까지 정수의 합은 " + tot);
---
+++ source/01_java/ch06_method/src/com/lec/method/Arithmetic.java
@@ -0,0 +1,28 @@
+package com.lec.method;
+// sum(10, 100), sum(10) - 함수 오버로딩 / evenOdd(10) / abs(-9)
+public class Arithmetic {
+	public int sum(int from, int to) { // from부터 to까지 누적합 return
+		int sum = 0; // 누적변수
+		for(int i=from ; i<=to ; i++) {
+			sum += i; // sum = sum+i;
+		}
+		return sum;
+	}
+	public int sum(int to) { // 1부터 to까지 누적합 return
+		int result = 0;
+		for(int i=1 ; i<=to ; i++) {
+			result += i;
+		}
+		return result;
+	}
+	
+	public String evenOdd(int value) { // value가 짝수인지 홀수인지를 return
+		String result = (value%2==0) ? "짝수입니다" : "홀수입니다";
+		return result;
+//		if (value%2==0) {
+//			return "짝수입니다";
+//		}else {
+//			return "홀수입니다";
+//		}
+	}
+}
