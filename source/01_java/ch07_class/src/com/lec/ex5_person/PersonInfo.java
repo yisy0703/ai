5fe72f1862f932f71b82148af66f105ea8ce48a2
yisy0703 <yisy7003@naver.com>
2024-09-03 12:16:17 +0900

    Java class

--- source/01_java/ch07_class/src/com/lec/ex5_person/PersonInfo.java
+++ source/01_java/ch07_class/src/com/lec/ex5_person/PersonInfo.java
@@ -1,6 +1,6 @@
 package com.lec.ex5_person;
 // 데이터 : name, age, gender, birth(String=>13장 수업후에는 Date형)
-// 생성자 : new PersonInfo("홍길동", 22, '남', "2001-12-01"), new PersonInfo("홍길동", 22, '남') - 오버로딩
+// 생성자 : new PersonInfo("홍길동", 22, 'm', "2001-12-01"), new PersonInfo("홍길동", 22, 'm') - 오버로딩
 // 메소드 : print()
 public class PersonInfo {
 	private String 	name;
@@ -21,13 +21,46 @@ public class PersonInfo {
 	public void print() { // 객체 정보 출력
 		System.out.println("이름 : " + name);
 		System.out.println("나이 : " + age);
-		System.out.println("성별 : " + gender);
+		System.out.println("성별 : " + (gender=='m' ? "남자":"여자") );
 		System.out.println("생년월일 : " + birth);
 	}
 	public String infoString() { // 객체 정보를 스트링으로 바꿔서 return
-		return    "이름 : " + name +
-				"\n나이 : " + age + 
-				"\n성별 : " + gender + 
-				"\n생년월일 : " + birth;
+		String result = "이름 : " + name ;
+		result += "\n나이 : " + age;
+		result += "\n성별 : " + (gender=='m' ? "남자":"여자");
+		result += "\n생년월일 : " + birth;
+		return result;
+//		return    "이름 : " + name +
+//				"\n나이 : " + age + 
+//				"\n성별 : " + (gender=='m' ? "남자":"여자") + 
+//				"\n생년월일 : " + birth;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public char getGender() {
+		return gender;
+	}
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+	public String getBirth() {
+		return birth;
+	}
+	public void setBirth(String birth) {
+		this.birth = birth;
 	}
 }
+
+
+
+
