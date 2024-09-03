313d73614f95c43a17a62d083b982e656cb05bdb
yisy0703 <yisy7003@naver.com>
2024-09-03 15:31:13 +0900

    Java static

---
+++ source/01_java/ch08_accessStatic/src/com/lec/ex3_access/AccessTest.java
@@ -0,0 +1,20 @@
+package com.lec.ex3_access;
+
+public class AccessTest {
+	private int privateMember; // 같은 클래스 내에서만 사용 가능
+	        int defaultMember; // 디폴트 접근 제한 : 같은 패키지에서만 사용 가능
+	protected int protectedMember; // 같은 패키지나 상속받은 클래스에서만 사용 가능
+	public  int publicMember;  // 아무데서나 사용 가능
+	private void privateMethod() {
+		System.out.println("private 메소드");
+	}
+	void defaultMethod() {
+		System.out.println("default 메소드");
+	}
+	protected void protectedMethod() {
+		System.out.println("protected 메소드");
+	}
+	public void publicMethod() {
+		System.out.println("public 메소드");
+	}
+}
