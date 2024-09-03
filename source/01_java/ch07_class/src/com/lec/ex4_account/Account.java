546830f52c8a5b4bf03445b4a8b88346e1a05798
yisy0703 <yisy7003@naver.com>
2024-09-03 10:06:10 +0900

    Java class

---
+++ source/01_java/ch07_class/src/com/lec/ex4_account/Account.java
@@ -0,0 +1,8 @@
+package com.lec.ex4_account;
+// 데이터(인스턴스변수) : accountNo(String), ownerName(String), balance(long)
+// 생성자 new Account("010-777-77", "홍길동"), new Account("010-777-77", "홍길동", 100)
+// 기능(메소드) : deposit(예금), withdraw(인출), infoPrint(정보출력), infoString(정보문자열 반환)
+public class Account {
+	private String accountNo; // 계좌번호
+	private String ownerName; // 예금주
+}
