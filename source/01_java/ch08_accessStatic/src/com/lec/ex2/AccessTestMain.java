f54588a84abed004c7cf92008b63e65d602ece26
yisy0703 <yisy7003@naver.com>
2024-09-03 15:54:14 +0900

    Java static

--- source/01_java/ch08_accessStatic/src/com/lec/ex2/AccessTestMain.java
+++ source/01_java/ch08_accessStatic/src/com/lec/ex2/AccessTestMain.java
@@ -1,15 +1,17 @@
 package com.lec.ex2;
 
+import com.lec.ex2_access.AccessTest;
+
 public class AccessTestMain {
 	public static void main(String[] args) {
 		AccessTest obj = new AccessTest();
 		//System.out.println(obj.privateMember);
-		System.out.println(obj.defaultMember);
-		System.out.println(obj.protectedMember);
+//		System.out.println(obj.defaultMember);
+//		System.out.println(obj.protectedMember);
 		System.out.println(obj.publicMember);
 		// obj.privateMethod();
-		obj.defaultMethod();
-		obj.protectedMethod();
+//		obj.defaultMethod();
+//		obj.protectedMethod();
 		obj.publicMethod();
 	}
 }
