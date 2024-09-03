61009859643d6f6c6eb65a72051513f37d49e5a8
yisy0703 <yisy7003@naver.com>
2024-09-03 12:28:12 +0900

    Java class

--- source/01_java/ch07_class/src/com/lec/ex5_person/PersonInfoTestMain2.java
+++ source/01_java/ch07_class/src/com/lec/ex5_person/PersonInfoTestMain2.java
@@ -1,12 +1,10 @@
 package com.lec.ex5_person;
 
-public class PersonInfoTestMain {
+public class PersonInfoTestMain2 {
 	public static void main(String[] args) {
 		int i = 10;
 		int[] scores = {i,100,95};
-		PersonInfo p1 = new PersonInfo("홍길동", 33, 'm');
-		PersonInfo p2 = new PersonInfo("홍길순", 32, 'f', "2001-12-01");
-		PersonInfo[] person = {p1, p2};
+		PersonInfo[] person = {new PersonInfo("홍길동", 33, 'm'), new PersonInfo("홍길순", 32, 'f', "2001-12-01")};
 		for(int idx=0 ; idx<person.length ; idx++) {
 			// person[idx].print();
 			System.out.println(person[idx].infoString());
