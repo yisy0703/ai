3e2bcccd961a7558f9b62ffb9141c58de1ea1a5a
yisy0703 <yisy7003@naver.com>
2024-09-03 17:49:25 +0900

    Java static

--- source/01_java/ch08_accessStatic/src/com/lec/ex5_final/FinalTestMain.java
+++ source/01_java/ch08_accessStatic/src/com/lec/ex5_final/FinalTestMain.java
@@ -1,11 +1,13 @@
 package com.lec.ex5_final;
+
+import constant.Constant;
 // final 변수 : 수정 불가
 public class FinalTestMain {
 	public static void main(String[] args) {
-		final double PI = 3.141592;
+		// final double PI = 3.141592;
 		double r = 3;
 		r = 5;
-		double area = r* r * PI;
+		double area = r* r * Constant.PI;
 		System.out.println(area);
 	}
 }
---
+++ source/01_java/ch08_accessStatic/src/constant/Constant.java
@@ -0,0 +1,5 @@
+package constant;
+
+public class Constant {
+	public static final double PI = 3.141592;
+}
