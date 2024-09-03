65544ce4f5eb97fd084aca56a5ee8dfab87563c5
yisy0703 <yisy7003@naver.com>
2024-09-03 12:46:11 +0900

    Java class

---
+++ source/01_java/ch08_accessStatic/src/com/lec/ex0_janmuri/Janmuri.java
@@ -0,0 +1,17 @@
+package com.lec.ex0_janmuri;
+class PersonInfo{
+	private String name;
+	private String tel;
+	public PersonInfo(String name, String tel) {
+		this.name = name;
+		this.tel  = tel;
+	}
+	public String info() {
+		return name + ":" + tel;
+	}
+}
+public class Janmuri {
+	public static void main(String[] args) {
+		PersonInfo p = new PersonInfo();
+	}
+}
