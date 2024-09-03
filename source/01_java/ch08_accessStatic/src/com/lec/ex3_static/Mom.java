5b4917d3e97f418d3a5328aba11512081032cf16
yisy0703 <yisy7003@naver.com>
2024-09-03 16:30:28 +0900

    Java static

--- source/01_java/ch08_accessStatic/src/com/lec/ex3_static/Mom.java
+++ source/01_java/ch08_accessStatic/src/com/lec/ex3_static/Mom.java
@@ -1,7 +1,7 @@
 package com.lec.ex3_static;
 
 public class Mom {
-	int money = 2000;
+	int money = 2000; // 데이터 초기화 방법2
 //	public Mom() { // 데이터 초기화 방법1 : 생성자 이용
 //		money = 2000;
 //	}
---
+++ source/01_java/ch08_accessStatic/src/com/lec/ex3_static/MomTestMain.java
@@ -0,0 +1,14 @@
+package com.lec.ex3_static;
+
+public class MomTestMain {
+	public static void main(String[] args) {
+		Child child1 = new Child("첫째 길동이");
+		Child child2 = new Child("둘째 개똥이");
+		Child child3 = new Child("셋째 똘순이");
+		System.out.println("첫째 엄마 지갑 : " + child1.mom.money);
+		child1.takeMoney(1000);
+		child2.takeMoney(1000);
+		child3.takeMoney(1000);
+		System.out.println("첫째 엄마 지갑 : " + child1.mom.money);
+	}
+}
