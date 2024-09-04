5d94a6c61425365d2a18b36f8c014b90a8c32431
yisy0703 <yisy0703@naver.com>
2024-09-04 12:42:09 +0900

    Java inheritance

--- source/01_java/ch09_inheritance/src/com/lec/ex1_parentChild/ChildClass.java
+++ source/01_java/ch09_inheritance/src/com/lec/ex1_parentChild/ChildClass.java
@@ -1,5 +1,8 @@
 package com.lec.ex1_parentChild;
-
-public class ChildClass {
-
+//pStr, getPapaName(), getMamiName()
+public class ChildClass extends ParentClass{
+	String cStr = "자식클래스";
+	public ChildClass() {
+		System.out.println("자식 클래스 생성자");
+	}
 }
---
+++ source/01_java/ch09_inheritance/src/com/lec/ex1_parentChild/ChildClassTestMain.java
@@ -0,0 +1,7 @@
+package com.lec.ex1_parentChild;
+
+public class ChildClassTestMain {
+	public static void main(String[] args) {
+		ChildClass child = new ChildClass();
+	}
+}
