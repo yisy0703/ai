85028e0e674ed35f0ea6b60d1c18146e3919449c
yisy0703 <yisy0703@naver.com>
2024-09-04 17:35:08 +0900

    Java inheritance

---
+++ source/01_java/ch09_inheritance/src/com/lec/ex7_customer/Constant.java
@@ -0,0 +1,5 @@
+package com.lec.ex7_customer;
+public class Constant {
+	public static final int VIPLIMIT = 1000000;
+	public static final double POINTRATE = 0.01;
+}
--- source/01_java/ch09_inheritance/src/com/lec/ex7_customer/Customer.java
+++ source/01_java/ch09_inheritance/src/com/lec/ex7_customer/Customer.java
@@ -1,5 +1,38 @@
 package com.lec.ex7_customer;
-// new Customer("홍", "010-9999-9999","서울","12-12")
+// Customer c = new Customer("홍", "010-9999-9999","서울","12-12")
+// c.buy(1200)
 public class Customer extends Person{
-	
+	private String address;
+	private int sum; // 구매금액 누적
+	private int point; // 포인트 (구매금액의 1%)
+	private String date; // String을 다음주부터는 날짜형
+	private boolean vip; // true면 vip고객, false면 일반고객
+	public Customer(String name, String tel, String address, String date) {
+		super(name, tel);
+		this.address = address;
+		this.date = date;
+		point = 1000;
+		System.out.println(name + "님 가입 감사 포인트 무려 1000점 드렸습니다");
+	}
+	public void buy(int price) {
+		sum += price; // 구매금액 누적
+		if(vip==false && sum>=1000000) {
+			vip = true;
+			System.out.println(getName() + "님 VIP로 업그레이드 되셨습니다 짱입니다");
+		}
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
