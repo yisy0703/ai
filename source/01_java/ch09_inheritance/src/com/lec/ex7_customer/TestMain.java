315d7a1fb683f95714f2570f173c6cb4fd710bb9
yisy0703 <yisy0703@naver.com>
2024-09-04 18:05:02 +0900

    Java inheritance

--- source/01_java/ch09_inheritance/src/com/lec/ex7_customer/TestMain.java
+++ source/01_java/ch09_inheritance/src/com/lec/ex7_customer/TestMain.java
@@ -1,10 +1,14 @@
 package com.lec.ex7_customer;
-
 public class TestMain {
 	public static void main(String[] args) {
 		Customer customer = new Customer("홍길동", "010-9999-9999", "서울", "01-01");
 		customer.buy(500000);
 		System.out.println("----------------");
 		customer.buy(500000);
+		Staff staff = new Staff("홍사원", "010-8888-8888", "개발");
+		Person[] person = {customer, staff};
+		for(Person p : person) {
+			System.out.println(p.infoString());
+		}
 	}
 }
