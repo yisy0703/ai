3dfc1ff69898e38b3f1719e655804070f11f8be7
yisy0703 <yisy0703@naver.com>
2024-09-04 14:45:40 +0900

    Java inheritance

---
+++ source/01_java/ch09_inheritance/src/com/lec/ex4_override/Parent.java
@@ -0,0 +1,9 @@
+package com.lec.ex4_override;
+public class Parent {
+	public Parent() { // overloading = 함수 중복 정의
+		System.out.println("매개변수 없는 Parent 생성자");
+	}
+	public Parent(int i) {
+		System.out.println("매개변수 있는 Parent 생성자");
+	}
+}
