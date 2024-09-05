b199feb1d81788eadb06fa46983999b4fd6e74cb
yisy0703 <yisy0703@naver.com>
2024-09-05 10:55:06 +0900

    Java inheritance

--- source/01_java/ch09_inheritance/src/com/lec/ex8_store/StoreMain.java
+++ source/01_java/ch09_inheritance/src/com/lec/ex8_store/StoreMain.java
@@ -1,10 +1,16 @@
 package com.lec.ex8_store;
 public class StoreMain {
 	public static void main(String[] args) {
-		HeadQuarterStore head = new HeadQuarterStore("= = 본사 = =");
-		Store1 st1 = new Store1("= = 주택가 1호점 = =");
-		Store2 st2 = new Store2("= = 대학가 2호점 = =");
-		Store3 st3 = new Store3("= = 증권가 3호점 = =");
-		HeadQuarterStore[] stores = {head, st1, st2, st3};
+		HeadQuarterStore[] stores = { 
+				new HeadQuarterStore("= = 본사 = ="),
+				new Store1("= = 주택가 1호점 = ="),
+				new Store2("= = 대학가 2호점 = ="),
+				new Store3("= = 증권가 3호점 = =")
+		};
+//		HeadQuarterStore head = new HeadQuarterStore("= = 본사 = =");
+//		Store1 st1 = new Store1("= = 주택가 1호점 = =");
+//		Store2 st2 = new Store2("= = 대학가 2호점 = =");
+//		Store3 st3 = new Store3("= = 증권가 3호점 = =");
+//		HeadQuarterStore[] stores = {head, st1, st2, st3};
 	}
 }
