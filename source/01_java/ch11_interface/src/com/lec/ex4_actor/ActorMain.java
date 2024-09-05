686d1ae4ad897cee7a44e4ddd7a1a12b54a80294
yisy0703 <yisy0703@naver.com>
2024-09-05 17:55:13 +0900

    java interface

--- source/01_java/ch11_interface/src/com/lec/ex4_actor/ActorMain.java
+++ source/01_java/ch11_interface/src/com/lec/ex4_actor/ActorMain.java
@@ -23,6 +23,13 @@ public class ActorMain {
 		fighterPark.saveMan();
 //		fighterPark.makePizza();
 //		fighterPark.makeSpaghetti();
+		IChef chefPark = park;
+//		chefPark.canCatchCriminal();
+//		chefPark.canSearch();
+//		chefPark.outFire();
+//		chefPark.saveMan();
+		chefPark.makePizza();
+		chefPark.makeSpaghetti();
 	}
 }
 
