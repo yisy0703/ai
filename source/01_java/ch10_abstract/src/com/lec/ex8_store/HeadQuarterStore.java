b474514fa2afc329daba2395e5d354b132165853
yisy0703 <yisy0703@naver.com>
2024-09-05 11:45:54 +0900

    Java inheritance

--- source/01_java/ch10_abstract/src/com/lec/ex8_store/HeadQuarterStore.java
+++ source/01_java/ch10_abstract/src/com/lec/ex8_store/HeadQuarterStore.java
@@ -1,16 +1,17 @@
 package com.lec.ex8_store;
-//김치찌개-8,000  부대찌개-9,000  비빔밥-9,000 순대국-8,000  공기밥-1,000원
-// new HeadQuarterStore("본사") new Store1("1호점")
-public class HeadQuarterStore{
+// HeadQuarterStore를 상속받은 측에서 반드시 override 하도록
+// 추상클래스 : 추상메소드가 1개 이상
+public abstract class HeadQuarterStore{
 	private String storeName;
 	public HeadQuarterStore(String storeName) {
 		this.storeName = storeName;
 	}
-	public void kimchi(){ System.out.println("김치찌개 : 8,000원");}
-	public void bude()  { System.out.println("부대찌개 : 9,000원");}
-	public void bibim() { System.out.println("비빔밥 : 9,000원");	}
-	public void sunde() { System.out.println("순대국 : 8,000원");	}
-	public void gongi() { System.out.println("공기밥 : 1,000원");	}
+	// 추상메소드 : 메소드 선언만 있고, 구현은 없는 메소드(구현은 상속받은 측에서)
+	public abstract void kimchi();
+	public abstract void bude();
+	public abstract void bibim();
+	public abstract void sunde();
+	public abstract void gongi();
 	public String getStoreName() {return storeName;}
 	@Override
 	public String toString() {
