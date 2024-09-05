e20640e119a924fe67514ac2cab9cd1f64c8aa55
yisy0703 <yisy0703@naver.com>
2024-09-05 10:50:52 +0900

    Java inheritance

---
+++ source/01_java/ch09_inheritance/src/com/lec/ex8_store/Store3.java
@@ -0,0 +1,15 @@
+package com.lec.ex8_store;
+// 김치찌개-9,000(v) 부대찌개-10,000(v)  비빔밥-10,000(v) 얼큰순대국-9,000(v)  공기밥-1,000원
+public class Store3 extends HeadQuarterStore {
+	public Store3(String storeName) {
+		super(storeName);
+	}
+	@Override
+	public void kimchi() { System.out.println("김치찌개 : 9,000원"); }
+	@Override
+	public void bude() { System.out.println("부대찌개 : 9,000원"); }
+	@Override
+	public void bibim() {System.out.println("비빔밥 : 9,000원");}
+	@Override
+	public void sunde() {System.out.println("얼큰 순대국 : 9,000원");}
+}
