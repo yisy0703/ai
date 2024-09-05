e839e467a535c33dd59f5c9ed1cc7d1ca98301cd
yisy0703 <yisy0703@naver.com>
2024-09-05 11:12:58 +0900

    Java inheritance

--- source/01_java/ch09_inheritance/src/com/lec/ex8_store/StoreMain.java
+++ source/01_java/ch09_inheritance/src/com/lec/ex8_store/StoreMain.java
@@ -8,8 +8,13 @@ public class StoreMain {
 				new Store3("= = 증권가 3호점 = =")
 		};
 		for(int idx=0 ; idx<stores.length ; idx++) {
-			System.out.println(stores[idx].toString());
-		}
+			System.out.println(stores[idx]);
+			stores[idx].kimchi();
+			stores[idx].bude();
+			stores[idx].bibim();
+			stores[idx].sunde();
+			stores[idx].gongi();
+		} // 일반for
 //		HeadQuarterStore head = new HeadQuarterStore("= = 본사 = =");
 //		Store1 st1 = new Store1("= = 주택가 1호점 = =");
 //		Store2 st2 = new Store2("= = 대학가 2호점 = =");
