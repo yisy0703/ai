bfe4c626d5a977786c8666ab6e0580982abb3f18
yisy0703 <yisy0703@naver.com>
2024-09-05 17:38:39 +0900

    Java inheritance

---
+++ source/01_java/ch11_interface/src/com/lec/ex4_actor/ActorMain.java
@@ -0,0 +1,36 @@
+package com.lec.ex4_actor;
+// 다형성 ex
+public class ActorMain {
+	public static void main(String[] args) {
+		Actor park = new Actor("박보검");
+		park.canCatchCriminal();
+		park.canSearch();
+		park.outFire();
+		park.saveMan();
+		park.makePizza();
+		park.makeSpaghetti();
+		IPoliceMan policePark = park;
+		policePark.canCatchCriminal();
+		policePark.canSearch();
+//		policePark.outFire();
+//		policePark.saveMan();
+//		policePark.makePizza();
+//		policePark.makeSpaghetti();
+		IFireFighter fighterPark = park;
+//		fighterPark.canCatchCriminal();
+//		fighterPark.canSearch();
+		fighterPark.outFire();
+		fighterPark.saveMan();
+//		fighterPark.makePizza();
+//		fighterPark.makeSpaghetti();
+	}
+}
+
+
+
+
+
+
+
+
+
