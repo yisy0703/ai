148772a3a8dbcb7614436fd4b4bc2ca331385cd8
yisy0703 <yisy0703@naver.com>
2024-09-05 10:11:09 +0900

    java store

--- source/01_java/ch09_inheritance/src/com/lec/ex8_store/HeadQuarterStore.java
+++ source/01_java/ch09_inheritance/src/com/lec/ex8_store/HeadQuarterStore.java
@@ -1,5 +1,14 @@
 package com.lec.ex8_store;
 //김치찌개-8,000  부대찌개-9,000  비빔밥-9,000 순대국-8,000  공기밥-1,000원
-public class HeadQuarterStore {
-
+// new HeadQuarterStore("본사") new Store1("1호점")
+public class HeadQuarterStore{
+	private String storeName;
+	public HeadQuarterStore(String storeName) {
+		this.storeName = storeName;
+	}
+	public void kimchi(){ System.out.println("김치찌개 : 8,000원");}
+	public void bude()  { System.out.println("부대찌개 : 9,000원");}
+	public void bibim() { System.out.println("비빔밥 : 9,000원");	}
+	public void sunde() { System.out.println("순대국 : 8,000원");	}
+	public void gongi() { System.out.println("공기밥 : 1,000원");	}
 }
