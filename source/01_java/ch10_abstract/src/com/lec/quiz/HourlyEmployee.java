d986ef3d52babeddb0b9eb1b7e7402aa07848164
yisy0703 <yisy0703@naver.com>
2024-09-05 13:15:30 +0900

    Java inheritance

--- source/01_java/ch10_abstract/src/com/lec/quiz/HourlyEmployee.java
+++ source/01_java/ch10_abstract/src/com/lec/quiz/HourlyEmployee.java
@@ -1,5 +1,16 @@
 package com.lec.quiz;
 //name, hoursWorked, moneyPerHour, computePay(),computeIncentive()
 public class HourlyEmployee extends Employee{
+	private int hoursWorked;
+	private int moneyPerHour;
+	public HourlyEmployee(String name, int hoursWorked, int moneyPerHour) {
+		super(name);
+		this.hoursWorked = hoursWorked;
+		this.moneyPerHour = moneyPerHour;
+	}
+	@Override
+	public int computePay() {
+		return hoursWorked*moneyPerHour;
+	}
 
 }
