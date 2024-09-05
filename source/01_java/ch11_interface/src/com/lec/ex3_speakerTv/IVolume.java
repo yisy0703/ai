46fec5335f22adea8a71ddaf3cd39bb9a670d42d
yisy0703 <yisy0703@naver.com>
2024-09-05 16:35:13 +0900

    Java inheritance

--- source/01_java/ch11_interface/src/com/lec/ex3_speakerTv/IVolume.java
+++ source/01_java/ch11_interface/src/com/lec/ex3_speakerTv/IVolume.java
@@ -3,8 +3,11 @@ package com.lec.ex3_speakerTv;
 // 작업명세서 역할 ex
 // 인터페이스에는 static final 변수(상수)와 추상메소드(예외 : default메소드, static 메소드)
 public interface IVolume {
-	public void volumeUp(); 
-	public void volumeDown();
+	public void volumeUp();  // 볼륨이 1씩 증가
+	public void volumeUp(int i);  // 볼륨이 i씩 증가
+	public void volumeDown(); // 볼륨이 1씩 감소
+	public void volumeDown(int i); // 볼륨이 i씩 감소
+	
 	public default void setMute(boolean mute) { // 디폴트 메소드
 		if(mute) {
 			System.out.println("무음");
