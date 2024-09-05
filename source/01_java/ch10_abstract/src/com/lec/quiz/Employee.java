42711ba1e02d776dca4aaae9df29bc8b33b5db0a
yisy0703 <yisy0703@naver.com>
2024-09-05 13:07:46 +0900

    Java inheritance

--- source/01_java/ch10_abstract/src/com/lec/quiz/Employee.java
+++ source/01_java/ch10_abstract/src/com/lec/quiz/Employee.java
@@ -1,6 +1,24 @@
 package com.lec.quiz;
 //name, int computePay(추상메소드-월급을 return하는 메소드), 
 // int computeIncentive(월급이 300이상인 사람만 월급의 5%를 incentive)
-public class Employee {
-
+public abstract class Employee {
+	private String name;
+	public Employee(String name) {
+		this.name = name;
+	}
+	public abstract int computePay(); // 추상메소드(반드시 오버라이드)
+	public final int computeIncentive() { // final메소드(오버라이드 금지)
+		int pay = computePay();
+		if(pay >= 3000000) {
+			return (int)(pay*0.05);
+		}
+		return 0;
+	}
+	public String getName() {return name;}
 }
+
+
+
+
+
+
