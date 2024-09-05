82b82a6144624b27e26d4be2dcaf08aa9c921981
yisy0703 <yisy0703@naver.com>
2024-09-05 17:29:09 +0900

    Java inheritance

--- source/01_java/ch11_interface/src/com/lec/ex4_actor/Actor.java
+++ source/01_java/ch11_interface/src/com/lec/ex4_actor/Actor.java
@@ -2,4 +2,35 @@ package com.lec.ex4_actor;
 // new Actor("박보검")
 public class Actor implements IPoliceMan, IFireFighter, IChef{
 	private String name;
+	public Actor(String name) {
+		this.name = name;
+	}
+	@Override
+	public void makePizza() {
+		System.out.println(name + "은 요리사입니다. 파자를 굽습니다");
+	}
+	@Override
+	public void makeSpaghetti() {
+		System.out.println(name + "은 요리사입니다. 스파게티를 만듭니다");
+	}
+	@Override
+	public void outFire() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void saveMan() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void canCatchCriminal() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void canSearch() {
+		// TODO Auto-generated method stub
+		
+	}
 }
