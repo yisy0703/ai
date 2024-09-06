d13c48943a7060591e75ac335112fcb1fb4b1920
yisy0703 <yisy0703@naver.com>
2024-09-06 15:30:35 +0900

    Java inheritance

--- source/01_java/ch12_pattern/src/strategy1/step4/modularization/RobotTestMain.java
+++ source/01_java/ch12_pattern/src/strategy1/step4/modularization/RobotTestMain.java
@@ -1,4 +1,7 @@
 package strategy1.step4.modularization;
+
+import strategy1.step4.interfaces.FlyHigh;
+
 public class RobotTestMain {
 	public static void main(String[] args) {
 		SuperRobot    superRobot    = new SuperRobot();
@@ -14,6 +17,11 @@ public class RobotTestMain {
 			robot.actionKnife();
 		}//for
 		System.out.println("SuperRobot의 나는 부품을 높~~~~~~~이 나는 기능으로 업그레드");
+		superRobot.setFly(new FlyHigh());
+		superRobot.shape();
+		superRobot.actionFly();
+		robots[0].actionFly();
+		System.out.println("lowRobot의 knife부품을 '장난감 검이 있습니다'기능으로 업그레이드");
 	}//main
 }//class
 
