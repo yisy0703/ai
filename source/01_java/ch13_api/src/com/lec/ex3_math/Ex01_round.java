f028f40cb61a81e2cfd8675e0870dcc5c36a19e7
yisy0703 <yisy0703@naver.com>
2024-09-09 14:42:55 +0900

    Java api

--- source/01_java/ch13_api/src/com/lec/ex3_math/Ex01_round.java
+++ source/01_java/ch13_api/src/com/lec/ex3_math/Ex01_round.java
@@ -8,6 +8,16 @@ public class Ex01_round {
 		System.out.println("9.15를 올림 : " + (int)Math.ceil(9.15));
 		System.out.println("9.15를 반올림 : " + Math.round(9.15));
 		System.out.println("9.15를 버림 : " + (int)Math.floor(9.15));
+		
+		System.out.println("소수점 한자리에 반올림, 올림, 버림");
+		System.out.println("9.15를 올림 : " + Math.ceil(9.15*10)/10);
+		System.out.println("9.15를 반올림 : " + Math.round(9.15*10)/10.0);
+		System.out.println("9.15를 버림 : " + Math.floor(9.15*10)/10);
+		
+		System.out.println("십의 자리에 반올림, 올림, 버림");
+		System.out.println("69를 올림 : " + (int)Math.ceil(69/10.0)*10);
+		System.out.println("69를 반올림 : " + Math.round(69/10.0)*10);
+		System.out.println("69를 버림 : " + (int)Math.floor(69/10.0)*10);
 	}
 }
 
