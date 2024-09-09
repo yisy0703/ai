b46bd283c4186fb20af4241c6ab4724bbbab2515
yisy0703 <yisy0703@naver.com>
2024-09-09 10:22:07 +0900

    Java api

--- source/01_java/ch13_api/src/com/lec/ex1_String/Ex01.java
+++ source/01_java/ch13_api/src/com/lec/ex1_String/Ex01.java
@@ -1,5 +1,6 @@
 package com.lec.ex1_String;
-// 
+// 문자열변수에 문자열을 할당할 때, 이미 존재하는 문자열이 있으면 공유함.
+// 새롭게 문자열을 할당하면 기존 문자열 객체는 반환되고 새로 문자열 객체가 생성
 public class Ex01 {
 	public static void main(String[] args) {
 		int i = 10;
@@ -13,6 +14,12 @@ public class Ex01 {
 		}
 		System.out.println(str1==str3 ? "str1과 str3는 같은 주소를 참조":"str1과 str3는 다른 주소를 참조");
 		System.out.println(str1.equals(str3) ? "str1과 str3은 같은 문자열":"str1과 str3은 다른문자열");
+		str2 = "Pypi";
+		if(str1 == str2) {
+			System.out.println("str1과 str2는 같은 주소를 참조");
+		}else {
+			System.out.println("str1과 str2는 다른 주소를 참조");
+		}
 	}
 }
 
---
+++ source/01_java/ch13_api/src/com/lec/ex1_String/Ex02_StringApiMethod.java
@@ -0,0 +1,8 @@
+package com.lec.ex1_String;
+public class Ex02_StringApiMethod {
+	public static void main(String[] args) {
+		String str1 = "abcXabc";
+		String str2 = "AbCXabc";
+		String str3 = "   ja va   ";
+	}
+}
