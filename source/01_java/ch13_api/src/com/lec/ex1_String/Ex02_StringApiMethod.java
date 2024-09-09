4846503c3ae179a5c9485e2936f30638a454cbe5
yisy0703 <yisy0703@naver.com>
2024-09-09 11:09:17 +0900

    Java api

--- source/01_java/ch13_api/src/com/lec/ex1_String/Ex02_StringApiMethod.java
+++ source/01_java/ch13_api/src/com/lec/ex1_String/Ex02_StringApiMethod.java
@@ -4,5 +4,37 @@ public class Ex02_StringApiMethod {
 		String str1 = "abcXabc";
 		String str2 = "AbCXabc";
 		String str3 = "   ja va   ";
+		System.out.println("1." + str1.concat(str2)); // 1. abcXabcAbCXabc
+		System.out.println("2." + str1.substring(3)); // 2. Xabc
+		System.out.println("3." + str1.substring(3, 5)); // 3.Xa : 3번째부터 5번째앞까지
+		System.out.println("4." + str1.length());      // 4.7 : 문자열 길이
+		System.out.println("5." + str1.toUpperCase()); // 5.ABCXABC : 모두 대문자로
+		System.out.println("6." + str1.toLowerCase()); // 6.abcxabc
+		System.out.println("7." + str1.charAt(3));     // 7.X : 3번째인덱스 글자
+		System.out.println("8." + str1.indexOf("bc")); // 8.1 : 해당문자가 처음 오는 인덱스
+		String str4 = "abcXabcXabc";
+		System.out.println("9." + str4.indexOf("bc", 2));//9.5 : 해당문자를 2번째부터 검색
+		System.out.println("10." + str4.lastIndexOf("bc"));//10.9 : 마지막부터 해당문자열을 검색
+		System.out.println("11." + str4.indexOf("@")); // 11.-1 : 없는문자는 -1 반환
+		System.out.println("12." + str1.equals(str2)); // 12.false:비교
+		System.out.println("13." + str1.equalsIgnoreCase(str2));//13.true:대소문자구별없이 비교
+		System.out.println("14." + str3.trim()); //14.ja va :앞뒤space 제거
+		// 문자열.replace(oldStr, newStr)
+		// 문자열.replaceAll(oldStr정규표현식, newStr)
+		// [0-9] 숫자
+		System.out.println("15." + str1.replace("abc", "바꿔"));//15.바꿔X바꿔
+		String str5 = "반가워요. ㅋㅋ 또 봐요 ㅎㅎ zz";
+		System.out.println("16." + str5.replaceAll("[ㄱ-ㅎz]", ""));//해당정규표현식 문자 삭제
+		System.out.println("str1 = " + str1);
+		System.out.println("str2 = " + str2);
 	}
 }
+
+
+
+
+
+
+
+
+
---
+++ source/01_java/ch13_api/src/com/lec/ex1_String/Ex03_loop.java
@@ -0,0 +1,20 @@
+package com.lec.ex1_String;
+import java.util.Scanner;
+// I:입력로직, U:수정로직, X:종료
+public class Ex03_loop {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String fn;
+		do {
+			System.out.print("I:입력, U:수정, X:종료");
+			fn = scanner.next(); // "X"
+			switch (fn) {
+			case "I": 
+				System.out.println("입력로직수행");break;
+			case "U":
+				System.out.println("수정로직수행");
+			}
+		}while(! fn.equals("X"));
+		scanner.close();
+	}
+}
