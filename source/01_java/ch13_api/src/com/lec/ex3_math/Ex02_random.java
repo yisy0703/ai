dee79b37c04de43c0ab3d3c859915b5502e56ad5
yisy0703 <yisy0703@naver.com>
2024-09-09 14:46:02 +0900

    Java api

---
+++ source/01_java/ch13_api/src/com/lec/ex3_math/Ex02_random.java
@@ -0,0 +1,8 @@
+package com.lec.ex3_math;
+// 0 <= Math.random() < 1
+// 0 <= Math.random()*10 < 10
+public class Ex02_random {
+	public static void main(String[] args) {
+		System.out.println("0~9 사이의 정수 난수 : "+ (int)(Math.random()*10));
+	}
+}
