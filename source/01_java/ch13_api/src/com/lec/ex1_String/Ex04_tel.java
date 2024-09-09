f51d193fb733890c7ad63102cd0b45bcb0782519
yisy0703 <yisy0703@naver.com>
2024-09-09 11:36:14 +0900

    Java api

--- source/01_java/ch13_api/src/com/lec/ex1_String/Ex04_tel.java
+++ source/01_java/ch13_api/src/com/lec/ex1_String/Ex04_tel.java
@@ -1,5 +1,11 @@
 package com.lec.ex1_String;
-// quiz : 전화번호(02-7894-7897) 입력받아, 전번, 짝수번째문자, 꺼꾸로, 국번, 뒷4자리 출력
+import java.util.Scanner;
+// quiz : 전화번호(02-7894-7897) 입력받아, 전번, 짝수번째문자, 꺼꾸로, 국번, 뒷4자리 출력(x를입력할때까지 반복적)
 public class Ex04_tel {
-	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		while(true) {
+			
+		}
+	}
 }
