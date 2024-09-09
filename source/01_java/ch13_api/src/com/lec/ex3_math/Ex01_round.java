f47ac86369df2bc56e7a82f9ab39d8b6e91bb4a2
yisy0703 <yisy0703@naver.com>
2024-09-09 14:35:44 +0900

    Java api

---
+++ source/01_java/ch13_api/src/com/lec/ex3_math/Ex01_round.java
@@ -0,0 +1,28 @@
+package com.lec.ex3_math;
+// Math.round(소숫점 반올림할 실수) ex: Math.round(9.17) => 9(long형)
+// Math.ceil(소숫점 올릴 실수)		   Math.ceil(9.17)  => 10.0(double형)
+// Math.floor(소수점에서 버릴 실수)     Math.floor(9.17) => 9.0(double형)
+public class Ex01_round {
+	public static void main(String[] args) {
+		System.out.println("소수점에서 반올림, 올림, 버림");
+		System.out.println("9.15를 올림 : " + (int)Math.ceil(9.15));
+		System.out.println("9.15를 반올림 : " + Math.round(9.15));
+		System.out.println("9.15를 버림 : " + (int)Math.floor(9.15));
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
