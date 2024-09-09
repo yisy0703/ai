bd036077234caf2d9d2610008520dd4721a5b648
yisy0703 <yisy0703@naver.com>
2024-09-09 11:14:05 +0900

    Java api

--- source/01_java/ch13_api/src/com/lec/ex1_String/Ex03_loop.java
+++ source/01_java/ch13_api/src/com/lec/ex1_String/Ex03_loop.java
@@ -9,12 +9,20 @@ public class Ex03_loop {
 			System.out.print("I:입력, U:수정, X:종료");
 			fn = scanner.next(); // "X"
 			switch (fn) {
-			case "I": 
+			case "I": case "i":
 				System.out.println("입력로직수행");break;
-			case "U":
+			case "U": case "u":
 				System.out.println("수정로직수행");
 			}
-		}while(! fn.equals("X"));
+		}while(!fn.equals("X") && !fn.equals("x"));
 		scanner.close();
 	}
 }
+
+
+
+
+
+
+
+
