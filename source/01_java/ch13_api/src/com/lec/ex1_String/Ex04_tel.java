3f7f54868d2697262959f0c92867ea66861c7bcd
yisy0703 <yisy0703@naver.com>
2024-09-09 11:45:02 +0900

    Java api

--- source/01_java/ch13_api/src/com/lec/ex1_String/Ex04_tel.java
+++ source/01_java/ch13_api/src/com/lec/ex1_String/Ex04_tel.java
@@ -5,6 +5,18 @@ public class Ex04_tel {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		while(true) {
+			System.out.print("전화번호(ooo-oooo-oooo), 단 종료시 x : ");
+			String tel = scanner.next(); // "010-1234-5678"
+			if(tel.equalsIgnoreCase("x")) break; // 반복문 종료 조건
+			// 전화번호 출력 (tel출력)
+			System.out.println("전화번호 : " + tel);
+			// 짝수번째 문자열 출력 (length(), charAt(), 일반 for문 이용)
+			
+			// 꺼꾸로 출력(length(), charAt(), 일반 for문 이용)
+			
+			// 앞자리 출력 (indexOf(), substring() 이용)
+			
+			// 뒷자리 출력 (lastIndexOf(), substring() 이용)
 			
 		}
 	}
