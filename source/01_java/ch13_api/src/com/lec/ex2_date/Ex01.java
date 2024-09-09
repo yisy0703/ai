e6f226dbc5b546d16601d4d3e440daa7b35bd9ce
yisy0703 <yisy0703@naver.com>
2024-09-09 14:24:10 +0900

    Java api

--- source/01_java/ch13_api/src/com/lec/ex2_date/Ex01.java
+++ source/01_java/ch13_api/src/com/lec/ex2_date/Ex01.java
@@ -1,4 +1,5 @@
 package com.lec.ex2_date;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -10,5 +11,16 @@ public class Ex01 {
 		System.out.println(nowDate);
 		System.out.println(nowCal);
 		System.out.println(nowGc);
+		// 2024년 09월 09일 14시 14분 30초
+		// yyyy(년도 4자리) yy(년도 2자리), MM(월2자리), dd(일2자리) E(요일)
+		// a(오전/오후) HH(24시간 단위 2자리) hh(12시간 단위 2자리) mm(분 2자리) ss(초 2자리)
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 MM월 dd일(E) HH시 mm분 ss초");
+		String dateStr = sdf.format(nowDate);
+		// nowCal.getTime(): Calendar 객체를 date형으로 return
+		String calStr  = sdf.format(nowCal.getTime()); 
+		String gcStr = sdf.format(nowGc.getTime());
+		System.out.println(dateStr);
+		System.out.println(calStr);
+		System.out.println(gcStr);
 	}
 }
