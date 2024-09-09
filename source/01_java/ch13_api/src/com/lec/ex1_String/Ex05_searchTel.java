62020891a8584f49b0dd3bfc6247de7fde8e37d6
yisy0703 <yisy0703@naver.com>
2024-09-09 12:56:13 +0900

    Java api

---
+++ source/01_java/ch13_api/src/com/lec/ex1_String/Ex05_searchTel.java
@@ -0,0 +1,10 @@
+package com.lec.ex1_String;
+import java.util.Scanner;
+// 배열에 있는 전화번호 검색
+public class Ex05_searchTel {
+	public static void main(String[] args) {
+		String[] tel = {"010-9999-9999", "02-777-9999", "010-8888-8888"};
+		Scanner scanner = new Scanner(System.in);
+		boolean noSearch = true; // 검색결과가 없다고 디폴트 값. 검색결과가 있으면 true
+	}
+}
