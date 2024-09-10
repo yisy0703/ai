b0b9036da10acb280e162c15e364faa9590f9376
yisy0703 <yisy0703@naver.com>
2024-09-10 11:49:18 +0900

    Java exception

--- source/01_java/ch14_exception/src/com/lec/ex2_throws/ThrowsEx.java
+++ source/01_java/ch14_exception/src/com/lec/ex2_throws/ThrowsEx.java
@@ -1,5 +1,17 @@
 package com.lec.ex2_throws;
-
 public class ThrowsEx {
-
+	public ThrowsEx() {
+		actionB();
+	}
+	private void actionB() {
+		System.out.println("actionB 전반부");
+		actionA();
+		System.out.println("actionB 후반부");
+	}
+	private void actionA() {
+		System.out.println("actionA 전반부");
+		int[] arr = {0, 1, 3};
+		System.out.println(arr[3]);
+		System.out.println("actionA 후반후");
+	}
 }
