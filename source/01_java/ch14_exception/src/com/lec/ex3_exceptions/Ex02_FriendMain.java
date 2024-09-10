054a6a2e9a156c6b43bd9f5a9edfddd4b0577801
yisy0703 <yisy0703@naver.com>
2024-09-10 14:29:17 +0900

    Java exception

--- source/01_java/ch14_exception/src/com/lec/ex3_exceptions/Ex02_FriendMain.java
+++ source/01_java/ch14_exception/src/com/lec/ex3_exceptions/Ex02_FriendMain.java
@@ -2,7 +2,11 @@ package com.lec.ex3_exceptions;
 import java.util.Date;
 public class Ex02_FriendMain {
 	public static void main(String[] args) {
-		Friend friend1 = new Friend("황보길동","010-9999-9999", new Date(98, 8, 10));
+		Friend friend1 = new Friend("홍길동","010-9999-1234", new Date(98, 8, 10));
 		System.out.println(friend1);
+		Friend friend2 = new Friend("홍길동","010-9999-1234", new Date(98, 8, 9));
+		System.out.println(friend2);
+		Friend friend3 = new Friend("김길동");
+		System.out.println(friend3);
 	}
 }
--- source/01_java/ch14_exception/src/com/lec/ex3_exceptions/Friend.java
+++ source/01_java/ch14_exception/src/com/lec/ex3_exceptions/Friend.java
@@ -1,4 +1,5 @@
 package com.lec.ex3_exceptions;
+import java.text.SimpleDateFormat;
 // 데이터 : 이름,전화번호,생년월일(Date) 
 // Friend f = new Friend("홍길동", "010-9999-9999", new Date(98, 8, 10))
 // sysout(f) => [이름]홍길* [전화]***-****-9999 [생일]9월10일 ♬ 생일축하 ♬
@@ -22,8 +23,21 @@ public class Friend {
 	@Override
 	public String toString() {//[이름]홍길* [전화]***-****-9999 [생일]9월10일 ♬ 생일축하 ♬
 		String result = "";
+		// 이름
 		String nameOut = name.substring(0, name.length()-1) + "*";
 		result += "[이름]" + nameOut;
+		// 전화
+		String telOut = tel.substring(tel.lastIndexOf("-")+1);
+		result += " [전화]***-****-" + telOut;
+		// 생일
+		SimpleDateFormat sdf = new SimpleDateFormat("M월d일");
+		String birthOut = sdf.format(birthday);
+		result += " [생일]" + birthOut;
+		// 오늘이 생일인지 아닌지
+		String today = sdf.format(new Date()); // "9월10일"
+		if(today.equals(birthOut)) {
+			result += " ♬ 생일축하 ♬";
+		}
 		return result;
 	}
 }
