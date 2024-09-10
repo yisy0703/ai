342f0c91f06258515f6375dc8f5288ec59fda939
yisy0703 <yisy0703@naver.com>
2024-09-10 12:26:57 +0900

    Java exception

--- source/01_java/ch14_exception/src/com/lec/ex2_throws/ThrowsTestMain.java
+++ source/01_java/ch14_exception/src/com/lec/ex2_throws/ThrowsTestMain.java
@@ -1,7 +1,7 @@
 package com.lec.ex2_throws;
 
 public class ThrowsTestMain {
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		try {
 			new ThrowsEx();
 		} catch (Exception e) {
