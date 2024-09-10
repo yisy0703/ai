da865bb8cbf13579de5c644c7b173e8d3720f477
yisy0703 <yisy0703@naver.com>
2024-09-10 17:42:35 +0900

    Java exception

--- source/01_java/ch15_collection/src/com/lec/ex2_map/Ex01_HashMap.java
+++ source/01_java/ch15_collection/src/com/lec/ex2_map/Ex01_HashMap.java
@@ -1,5 +1,8 @@
 package com.lec.ex2_map;
 import java.util.HashMap;
+import java.util.Iterator;
+
+import com.lec.ex1_list.Friend;
 public class Ex01_HashMap {
 	public static void main(String[] args) {
 		HashMap<Integer, String> hashMap = new HashMap<Integer, String>();
@@ -7,5 +10,26 @@ public class Ex01_HashMap {
 		hashMap.put(12, "str12");
 		hashMap.put(22, "str33");
 		hashMap.put(20, "str20");
+		System.out.println(11 + "key값의 데이터 : " + hashMap.get(11));
+		System.out.println(10 + "key값의 데이터(없으면 null) : " + hashMap.get(10));
+		System.out.println(hashMap);
+		hashMap.remove(12); // remove시 key값으로 : 12key값의 데이터 제거
+		System.out.println("12 remove 후 : " + hashMap);
+		hashMap.clear(); // hashMap의 모든 데이터 제거
+		System.out.println(hashMap.isEmpty()? "데이터 없음":"데이터 있음");
+		// 출력
+		hashMap.put(0, "Hong giledong");
+		hashMap.put(10, "Kim dongil");
+		hashMap.put(22, "Lee soonsin");
+		hashMap.put(40, "Shin gildong");
+		Iterator<Integer> iterator = hashMap.keySet().iterator(); // 반복자
 	}
 }
+
+
+
+
+
+
+
+
