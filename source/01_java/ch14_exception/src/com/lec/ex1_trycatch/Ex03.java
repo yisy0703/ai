860551e33e59698911fe9f323b4dc8d86bb89f13
yisy0703 <yisy0703@naver.com>
2024-09-10 10:41:47 +0900

    Java exception

--- source/01_java/ch14_exception/src/com/lec/ex1_trycatch/Ex03.java
+++ source/01_java/ch14_exception/src/com/lec/ex1_trycatch/Ex03.java
@@ -1,17 +1,20 @@
 package com.lec.ex1_trycatch;
+import java.util.InputMismatchException;
 import java.util.Scanner;
+// 첫번째 정수를 제대로 입력할 때까지 입력받음, 두번째 정수를 제대로 입력하지 않으면 1로 처리하여 수행하도록
 public class Ex03 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("첫번째 정수는 ");
+		
 		int i=0, j=1;
 		try {
+			System.out.print("첫번째 정수는 ");
 			i = scanner.nextInt(); // InputMismatchException
 			System.out.print("두번째 정수는 ");
 			j = scanner.nextInt(); // InputMismatchException
 			System.out.println("i/j = " + (i/j));//ArithmeticException
-		}catch (Exception e) {
-			// TODO: handle exception
+		}catch (InputMismatchException e) {
+			System.out.println(e.getMessage());
 		}
 		System.out.println("i*j = " + (i*j));
 		System.out.println("i+j = " + (i+j));
