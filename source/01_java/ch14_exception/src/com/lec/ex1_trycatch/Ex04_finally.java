d938d3da83bdc90767495866890c1bd2c41fc439
yisy0703 <yisy0703@naver.com>
2024-09-10 11:37:31 +0900

    Java exception

--- source/01_java/ch14_exception/src/com/lec/ex1_trycatch/Ex04_finally.java
+++ source/01_java/ch14_exception/src/com/lec/ex1_trycatch/Ex04_finally.java
@@ -2,5 +2,14 @@ package com.lec.ex1_trycatch;
 public class Ex04_finally {
 	public static void main(String[] args) {
 		int[] arr = {0, 1, 2};
+		for(int i=0 ; i<=arr.length ; i++) {
+			try {
+				System.out.println("arr[" + i + "] = " + arr[i]);
+			}catch (ArrayIndexOutOfBoundsException e) {
+				System.out.println("예외 메세지 : " + e.getMessage());
+			}finally {
+				System.out.println("try절 실행후에도, catch 절 실행후에도 실행되는 절\n");
+			}
+		}
 	}
 }
