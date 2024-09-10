acabaeff593e80eb291d2936ec1e2bf42542ec21
yisy0703 <yisy0703@naver.com>
2024-09-10 17:23:49 +0900

    Java exception

---
+++ source/01_java/ch15_collection/src/com/lec/ex2_map/Ex01_HashMap.java
@@ -0,0 +1,11 @@
+package com.lec.ex2_map;
+import java.util.HashMap;
+public class Ex01_HashMap {
+	public static void main(String[] args) {
+		HashMap<Integer, String> hashMap = new HashMap<Integer, String>();
+		hashMap.put(11, "str11"); // key11에 데이터 "str11" 추가
+		hashMap.put(12, "str12");
+		hashMap.put(22, "str33");
+		hashMap.put(20, "str20");
+	}
+}
