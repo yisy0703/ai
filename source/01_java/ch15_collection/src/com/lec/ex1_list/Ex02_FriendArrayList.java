8679b90492d6cebe5161a3cb3a344b642b14e330
yisy0703 <yisy0703@naver.com>
2024-09-10 16:40:39 +0900

    Java exception

---
+++ source/01_java/ch15_collection/src/com/lec/ex1_list/Ex02_FriendArrayList.java
@@ -0,0 +1,8 @@
+package com.lec.ex1_list;
+public class Ex02_FriendArrayList {
+	public static void main(String[] args) {
+		Friend[] fs = new Friend[2];
+		fs[0] = new Friend("홍길동", "010-7894-7894");
+		fs[1] = new Friend("신길동", "010-8888-8888");
+	}
+}
---
+++ source/01_java/ch15_collection/src/com/lec/ex1_list/Friend.java
@@ -0,0 +1,20 @@
+package com.lec.ex1_list;
+// new Friend("홍길동","010-7897-7897")
+public class Friend {
+	private String name;
+	private String tel;
+	public Friend(String name, String tel) {
+		this.name = name;
+		this.tel = tel;
+	}
+	@Override
+	public String toString() {
+		return "[name=" + name + ", tel=" + tel + "]";
+	}
+	public String getName() {
+		return name;
+	}
+	public String getTel() {
+		return tel;
+	}
+}
