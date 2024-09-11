eae116a43c971dc2f8945b095f6d074d775569f0
yisy0703 <yisy0703@naver.com>
2024-09-11 09:58:19 +0900

    Java collection

--- source/01_java/ch15_collection/src/com/lec/ex2_map/Ex02_FriendHashMap.java
+++ source/01_java/ch15_collection/src/com/lec/ex2_map/Ex02_FriendHashMap.java
@@ -1,6 +1,8 @@
 package com.lec.ex2_map;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+
 import com.lec.ex1_list.Friend;
 public class Ex02_FriendHashMap {
 	public static void main(String[] args) {
@@ -11,5 +13,15 @@ public class Ex02_FriendHashMap {
 		friends.put(newFriend.getTel(), newFriend);
 		friends.put("010-9999-9999", new Friend("홍길동", "010-9999-9999"));
 		friends.put("010-7777-7777", new Friend("신길동", "010-7777-7777"));
+		Iterator<String> iterator = friends.keySet().iterator(); // 반복자
+		while(iterator.hasNext()) {
+			String key = iterator.next();
+			System.out.println(friends.get(key).getName() +"\t" + friends.get(key).getTel());
+		}
+		iterator = friends.keySet().iterator(); // 반복자
+		while(iterator.hasNext()) {
+			String key = iterator.next();
+			System.out.println(friends.get(key));
+		}
 	}
 }
