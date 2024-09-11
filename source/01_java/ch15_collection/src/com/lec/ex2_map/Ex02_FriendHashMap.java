1fcf9ffcd840b521f6fc291566574d9c2430cb81
yisy0703 <yisy0703@naver.com>
2024-09-11 09:50:42 +0900

    Java collection

---
+++ source/01_java/ch15_collection/src/com/lec/ex2_map/Ex02_FriendHashMap.java
@@ -0,0 +1,15 @@
+package com.lec.ex2_map;
+import java.util.ArrayList;
+import java.util.HashMap;
+import com.lec.ex1_list.Friend;
+public class Ex02_FriendHashMap {
+	public static void main(String[] args) {
+		Friend[] farr = new Friend[5];
+		ArrayList<Friend> fs = new ArrayList<Friend>();
+		HashMap<String, Friend> friends = new HashMap<String, Friend>();
+		Friend newFriend = new Friend("김길동", "010-8888-9999");
+		friends.put(newFriend.getTel(), newFriend);
+		friends.put("010-9999-9999", new Friend("홍길동", "010-9999-9999"));
+		friends.put("010-7777-7777", new Friend("신길동", "010-7777-7777"));
+	}
+}
