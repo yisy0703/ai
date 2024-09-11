462abd857a53be4ab005099f710c6833fe7b1091
yisy0703 <yisy0703@naver.com>
2024-09-11 15:36:04 +0900

    Java collection

--- source/01_java/ch16_io/src/com/lec/ex1_inputstreamOutputstream/Ex01_InputStream.java
+++ source/01_java/ch16_io/src/com/lec/ex1_inputstreamOutputstream/Ex01_InputStream.java
@@ -1,5 +1,23 @@
 package com.lec.ex1_inputstreamOutputstream;
-// 파일 input : 
+import java.io.*;
+// (1)파일을 연다(스트림객체생성) (2)데이터를읽는다(read메소드사용) (3)파일을 닫는다(close메소드사용)
 public class Ex01_InputStream {
-
+	public static void main(String[] args) {
+		try {
+			InputStream is = new FileInputStream("txtFile/inTest.txt");//1.파일열기
+			// 2. 데이터읽는다(1byte씩 파일 끝까지)
+			while(true) {
+				int i = is.read(); // 1byte 씩 읽는다
+				if(i == -1) break; // 파일의 끝이면 break;
+				System.out.print((char)i);
+			}
+			System.out.println("끝");
+		} catch (FileNotFoundException e) {
+			System.out.println("예외메세지 : 파일 못 찾음. "+e.getMessage());
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			
+		}
+	}
 }
