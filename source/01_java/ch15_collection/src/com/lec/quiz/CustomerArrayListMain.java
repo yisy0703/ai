ccc8821e6a032500d5adac08bbfcf1f1c0a5447d
yisy0703 <yisy0703@naver.com>
2024-09-11 14:11:04 +0900

    Java collection

--- source/01_java/ch15_collection/src/com/lec/quiz/CustomerArrayListMain.java
+++ source/01_java/ch15_collection/src/com/lec/quiz/CustomerArrayListMain.java
@@ -6,29 +6,31 @@ public class CustomerArrayListMain {
 		ArrayList<Customer> customers = new ArrayList<Customer>();
 		Scanner scanner = new Scanner(System.in);
 		String answer=null, name, tel, address;
-		do {
+		while(true) {
 			System.out.print("회원가입 진행하시겠습니까 (Y/N)?");
 			answer = scanner.next();
 			if(answer.equalsIgnoreCase("y")) {
 				Customer newCustomer = new Customer();
-				System.out.print("가입하실 회원님 이름은?");
+				System.out.print("이름은?");
 				newCustomer.setName(scanner.next());
-				System.out.print("가입하실 회원님 전화는?");
+				System.out.print("전화 ?");
 				newCustomer.setTel(scanner.next());
-				System.out.print("가입하실 회원님 주소는?");
-				scanner.nextLine();// 버퍼지우는 용도
+				scanner.nextLine(); // 버퍼 지우기
+				System.out.print("주소?");
 				newCustomer.setAddress(scanner.nextLine());
 				customers.add(newCustomer);
-//				System.out.print("가입하실 회원님 이름은?");
+//				System.out.print("이름은?");
 //				name = scanner.next();
-//				System.out.print("가입하실 회원님 전화번호는 ?");
+//				System.out.print("전화?");
 //				tel = scanner.next();
-//				System.out.print("가입하실 회원님 주소는 ?");
-//				scanner.nextLine(); // 버퍼지우는 용도
+//				System.out.print("주소?");
+//				scanner.nextLine(); // 버퍼 지우기
 //				address = scanner.nextLine();
 //				customers.add(new Customer(name, tel, address));
+			}else if(answer.equalsIgnoreCase("n")) {
+				break;
 			}
-		}while(!answer.equalsIgnoreCase("n"));
+		}
 		scanner.close();
 		// N(n)을 입력할 경우 회원정보 출력
 //		if(customers.isEmpty()) {
