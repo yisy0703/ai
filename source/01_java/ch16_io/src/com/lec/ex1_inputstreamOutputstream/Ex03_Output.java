16748ed51047e76c379940ccccd980c2f0bfac19
yisy0703 <yisy0703@naver.com>
2024-09-11 17:02:29 +0900

    Java collection

--- source/01_java/ch16_io/src/com/lec/ex1_inputstreamOutputstream/Ex03_Output.java
+++ source/01_java/ch16_io/src/com/lec/ex1_inputstreamOutputstream/Ex03_Output.java
@@ -8,12 +8,12 @@ public class Ex03_Output {
 	public static void main(String[] args) {
 		OutputStream os = null;
 		try {
-			// 1. 파일 열기
-			os = new FileOutputStream("txtFile/outTest.txt"); // 있는 파일은 덮어쓰고, 없는 파일은 생성
+			// 1. 파일 열기 : true:append, 생략하거나 false :덮어쓰기
+			os = new FileOutputStream("txtFile/outTest.txt", true);  
 			// byte[] bs = {'H', 'e', 'l','l','o',',', ' '};
-			String msg = "Hello, World\nHi, AI\n";
+			String msg = "DBMS 첫날\n";
 			byte[] bs = msg.getBytes(); // 스트링을 byte배열로
-			// 2. 데이터 쓰기
+			// 2. 파일에 데이터 쓰기
 			os.write(bs);
 			System.out.println("파일 출력 성공");
 		} catch (IOException e) {
---
+++ source/01_java/ch16_io/src/com/lec/ex1_inputstreamOutputstream/Ex04_fileCopy.java
@@ -0,0 +1,48 @@
+package com.lec.ex1_inputstreamOutputstream;
+import java.io.*;
+//  ~/bts_7.mp4 => ~/bts_7_copy.mp4 (7,460,545 byte)
+public class Ex04_fileCopy {
+	public static void main(String[] args) {
+		long start = System.currentTimeMillis(); // 1970.1.1부터 시작시점까지의 밀리세컨
+		InputStream  is = null;
+		OutputStream os = null;
+		try {
+			is = new FileInputStream("D:/ai/lecNote/01_java/bts_7.mp4");
+			os = new FileOutputStream("D:\\ai\\lecNote\\01_java\\bts_copy.mp4");
+			int cnt = 0;
+			while(true) { // read & write
+				int i = is.read(); 
+				if(i == -1) break;
+				os.write(i);
+				cnt++;
+				if(cnt % 300000 == 0) {
+					System.out.println(cnt + "/7,460,545 번");
+				}
+			}
+			System.out.println("파일 복사 완료");
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			try {
+				if(is!=null) is.close();
+				if(os!=null) os.close()
+			} catch (IOException e2) {
+				// TODO: handle exception
+			}
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		long end = System.currentTimeMillis(); // 1970.1.1부터 종료시점까지의 밀리세컨
+		System.out.println((end-start)/1000.0 + "초 걸림");
+	}
+}
