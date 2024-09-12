c745b91771b4e69dfd2e6cf3a42bc2da0d35fb22
yisy0703 <yisy0703@naver.com>
2024-09-12 12:23:34 +0900

    select

--- source/02_DBMS/2_SELECT.sql
+++ source/02_DBMS/2_SELECT.sql
@@ -1,3 +1,6 @@
 -- [II] SELECT 문 - 조회
 
--- 1. SELECT 문장 작성법
+-- 1. SELECT 문장 작성법 (실행 : CTRL+ENTER / 블록잡고F9)
+SHOW USER;
+SELECT * FROM TAB; -- 현 계정이 갖고 있는 테이블들
+SELECT * FROM EMP; -- EMP테이블의 모든 열(필드), 모든 행
