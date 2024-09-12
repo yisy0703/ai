24b5b46acf28432d1fddfc69b58cd85b692b30d6
yisy0703 <yisy0703@naver.com>
2024-09-12 15:49:21 +0900

    select

--- source/02_DBMS/2_SELECT.sql
+++ source/02_DBMS/2_SELECT.sql
@@ -73,31 +73,43 @@ SELECT ENAME || '은 ' || JOB "EMPLOYEE" FROM EMP;
 SELECT DISTINCT DEPTNO FROM EMP;
 SELECT DISTINCT JOB FROM EMP;
 
+-- ★ 연습문제 꼭 풀기
 --1. emp 테이블의 구조 출력
-DESC EMP;
+DESC EMP; 
 --2. emp 테이블의 모든 내용을 출력 
-
+SELECT * FROM EMP;
 --3. 현 scott 계정에서 사용가능한 테이블 출력
-
+SHOW USER;
+SELECT * FROM TAB;
 --4. emp 테이블에서 사번, 이름, 급여, 업무, 입사일 출력
-
+SELECT EMPNO, ENAME, SAL, JOB, HIREDATE FROM EMP;
 --5. emp 테이블에서 급여가 2000미만인 사람의 사번, 이름, 급여 출력
-
+SELECT EMPNO, ENAME, SAL FROM EMP WHERE SAL<2000;
 --6. 입사일이 81/02이후에 입사한 사람의 사번, 이름, 업무, 입사일 출력
-
+SELECT EMPNO, ENAME, JOB, HIREDATE FROM EMP WHERE HIREDATE >= '81/02/01';
+-- 시스템의 날짜 FORMAT이 다를 경우는 다음과 같이 할 수 있으나, 4장에서 연습할 예정
+SELECT EMPNO, ENAME, JOB, HIREDATE FROM EMP 
+    WHERE TO_CHAR(HIREDATE,'RR/MM/DD') >= '81/02/01';
+SELECT EMPNO, ENAME, JOB, HIREDATE FROM EMP 
+    WHERE HIREDATE >= TO_DATE('81/02/01', 'RR/MM/DD');
 --7. 업무가 SALESMAN인 사람들 모든 자료 출력
-
+SELECT * FROM EMP WHERE JOB = 'SALESMAN';
 --8. 업무가 CLERK이 아닌 사람들 모든 자료 출력
-
+SELECT * FROM EMP WHERE JOB != 'CLERK';
+SELECT * FROM EMP WHERE JOB <> 'CLERK';
+SELECT * FROM EMP WHERE JOB ^= 'CLERK';
+SELECT * FROM EMP WHERE NOT JOB = 'CLERK';
 --9. 급여가 1500이상이고 3000이하인 사람의 사번, 이름, 급여 출력
-
+SELECT EMPNO, ENAME, SAL FROM EMP WHERE SAL>=1500 AND SAL<=3000;
 --10. 부서코드가 10번이거나 30인 사람의 사번, 이름, 업무, 부서코드 출력
-
+SELECT EMPNO, ENAME, JOB, DEPTNO FROM EMP WHERE DEPTNO=10 OR DEPTNO=30;
 --11. 업무가 SALESMAN이거나 급여가 3000이상인 사람의 사번, 이름, 업무, 부서코드 출력
-
+SELECT EMPNO, ENAME, JOB, DEPTNO FROM EMP WHERE JOB='SALESMAN' OR SAL>=3000;
 --12. 급여가 2500이상이고 업무가 MANAGER인 사람의 사번, 이름, 업무, 급여 출력
-
+SELECT EMPNO, ENAME, JOB, SAL FROM EMP WHERE SAL>=2500 AND JOB = 'MANAGER';
 --13.“ename은 XXX 업무이고 연봉은 XX다” 스타일로 모두 출력(연봉은 SAL*12+COMM)
+SELECT ENAME||'은(는) '||job||'업무이고 연봉은'||(SAL*12+NVL(COMM,0)) ||'다' MESSAGE 
+    FROM EMP;
 
 
 
