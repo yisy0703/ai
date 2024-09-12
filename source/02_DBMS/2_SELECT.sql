4ae5f717b5aef51c28063a672da508414752ce5f
yisy0703 <yisy0703@naver.com>
2024-09-12 13:11:39 +0900

    select

--- source/02_DBMS/2_SELECT.sql
+++ source/02_DBMS/2_SELECT.sql
@@ -4,3 +4,39 @@
 SHOW USER;
 SELECT * FROM TAB; -- 현 계정이 갖고 있는 테이블들
 SELECT * FROM EMP; -- EMP테이블의 모든 열(필드), 모든 행
+SELECT * FROM DEPT; -- DEPT테이블의 모든 열, 모든 행
+SELECT * FROM SALGRADE;
+-- 날짜 표기법 셋팅(RR/MM/DD)
+ALTER SESSION SET NLS_DATE_FORMAT = 'RR/MM/DD';
+
+-- 2. 특정 열만 출력
+DESC EMP;
+    -- EMP 테이블 구조
+SELECT EMPNO, ENAME, SAL, JOB FROM EMP; -- EMP테이블의 EMPNO, ENAME, SAL, JOB 필드만 모든 행
+SELECT EMPNO, ENAME, COMM FROM EMP;
+SELECT EMPNO AS "사 번", ENAME AS "이름", SAL AS "급여" FROM EMP; -- 필드명에 별칭을 사용
+SELECT EMPNO "사 번", ENAME "이름", SAL  "급여" FROM EMP; -- 별칭에 as 생략 가능
+SELECT EMPNO NO, ENAME NAME, SAL SALARY FROM EMP; -- 별칭에 SPACE가 없는 경우 " 생략가능
+SELECT EMPNO "NO", ENAME "NAME", SAL "SALARY" FROM EMP;
+
+-- 3. 특정 행 출력 : WHERE 절(조건절) -- 비교연산자 : 같다(=), 다르다(!=, ^= <> ), < <= > >=
+SELECT EMPNO 사번, ENAME 이름, SAL 급여 FROM EMP WHERE SAL=3000;
+SELECT * FROM EMP WHERE SAL<>3000;
+SELECT * FROM EMP WHERE SAL!=3000;
+SELECT * FROM EMP WHERE SAL^=3000;
+    -- 비교연산자는 숫자, 문자, 날짜형 모두 가능
+    -- 'A' < 'AA' < 'AAA' < 'AAAA' < 'AAAB' <'B'
+    -- EX1. 사원이름이 'A','B','C'로 시작하는 사원의 모든 필드
+    SELECT * FROM EMP WHERE ENAME < 'D';
+    -- EX2. 81년도 이전에 입사한 사원의 모든 필드
+
+
+
+
+
+
+
+
+
+
+
