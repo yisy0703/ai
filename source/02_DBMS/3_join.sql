b2906a2c6ce3df8d0bd1a3c7864938f3c1c79fee
yisy0703 <yisy0703@naver.com>
2024-09-13 16:08:06 +0900

    join

--- source/02_DBMS/3_join.sql
+++ source/02_DBMS/3_join.sql
@@ -170,7 +170,7 @@ SELECT WORKER.EMPNO, WORKER.ENAME, WORKER.MGR, MANAGER.EMPNO, MANAGER.ENAME
     SELECT E1.ENAME, E1.DEPTNO, E2.ENAME, E2.DEPTNO
         FROM EMP E1, EMP E2
         WHERE E1.ENAME='SCOTT' AND E1.DEPTNO=E2.DEPTNO AND E2.ENAME!='SCOTT';
-    -- 정답 :
+    -- (정답) :
     SELECT E2.ENAME
         FROM EMP E1, EMP E2
         WHERE E1.ENAME='SCOTT' AND E1.DEPTNO=E2.DEPTNO AND E2.ENAME!='SCOTT';
@@ -212,24 +212,45 @@ SELECT * FROM EMP E, DEPT D
 
 -- ★ ★ ★ <총 연습문제> ★ ★ ★ part2
 --1. 이름, 직속상사명
-
+SELECT W.ENAME, M.ENAME MANAGER
+    FROM EMP W, EMP M 
+    WHERE W.MGR=M.EMPNO;
 --2. 이름, 급여, 업무, 직속상사명
-
+SELECT W.ENAME, W.SAL, W.JOB, M.ENAME "MANAGER"
+    FROM EMP W, EMP M
+    WHERE W.MGR=M.EMPNO;
 --3. 이름, 급여, 업무, 직속상사명 . (상사가 없는 직원까지 전체 직원 다 출력.
     --상사가 없을 시 '없음'으로 출력)
-
+SELECT W.ENAME, W.SAL, W.JOB, NVL(M.ENAME,'없음') MANAGER
+    FROM EMP W, EMP M
+    WHERE W.MGR=M.EMPNO(+);
 --4. 이름, 급여, 부서명, 직속상사명
-
+SELECT W.ENAME, W.SAL, DNAME, M.ENAME MANAGER
+  FROM EMP W, EMP M, DEPT D
+  WHERE W.DEPTNO=D.DEPTNO AND W.MGR=M.EMPNO;
 --5. 상사가 없는 직원과 상사가 있는 직원 모두에 대해 이름, 급여, 부서코드, 부서명, 근무지, 직속상사명을 출력하시오(단, 직속상사가 없을 경우 직속상사명에는 ‘없음’으로 대신 출력하시오)
-
+SELECT W.ENAME, W.SAL, W.DEPTNO, DNAME, LOC, M.ENAME MANAGER
+  FROM EMP W, EMP M, DEPT D
+  WHERE W.DEPTNO=D.DEPTNO AND W.MGR=M.EMPNO(+);
 --6. 이름, 급여, 등급, 부서명, 직속상사명. 급여가 2000이상인 사람
-
+SELECT W.ENAME, W.SAL, GRADE, DNAME, M.ENAME MANAGER
+    FROM EMP W, SALGRADE, DEPT D, EMP M
+    WHERE W.SAL BETWEEN LOSAL AND HISAL AND W.DEPTNO=D.DEPTNO AND W.MGR=M.EMPNO
+        AND W.SAL >= 2000;  
 --7. 이름, 급여, 등급, 부서명, 직속상사명, (직속상사가 없는 직원까지 전체직원 부서명 순 정렬)
-
+SELECT W.ENAME, W.SAL, GRADE, DNAME, M.ENAME MANAGER
+  FROM EMP W, EMP M, DEPT D, SALGRADE
+  WHERE W.MGR=M.EMPNO(+) AND W.DEPTNO=D.DEPTNO AND W.SAL BETWEEN LOSAL AND HISAL
+  ORDER BY DNAME;
 --8. 이름, 급여, 등급, 부서명, 연봉, 직속상사명. 연봉=(급여+comm)*12으로 계산
-
+SELECT W.ENAME, W.SAL, GRADE, DNAME, (W.SAL+NVL(W.COMM, 0))*12 ANNUALSAL, M.ENAME MANAGER
+    FROM EMP W, EMP M, SALGRADE, DEPT D
+    WHERE W.MGR=M.EMPNO AND W.SAL BETWEEN LOSAL AND HISAL AND W.DEPTNO=D.DEPTNO;
 --9. 8번을 부서명 순 부서가 같으면 급여가 큰 순 정렬
-
+SELECT W.ENAME, W.SAL, GRADE, DNAME, (W.SAL+NVL(W.COMM, 0))*12 ANNUALSAL, M.ENAME MANAGER
+    FROM EMP W, EMP M, SALGRADE, DEPT D
+    WHERE W.MGR=M.EMPNO AND W.SAL BETWEEN LOSAL AND HISAL AND W.DEPTNO=D.DEPTNO;
+    
 --10. 사원테이블에서 사원명, 사원의 상사를 검색하시오(상사가 없는 직원까지 전체).
 
 --11. 사원명, 상사명, 상사의 상사명을 검색하시오(self join)
