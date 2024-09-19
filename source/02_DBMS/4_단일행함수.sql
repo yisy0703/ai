68f97b89b4234c8025d6c81af1c6d148d1e2e600
yisy0703 <yisy0703@naver.com>
2024-09-19 11:06:22 +0900

    Oracle 단일행함수

--- source/02_DBMS/4_단일행함수.sql
+++ source/02_DBMS/4_단일행함수.sql
@@ -35,9 +35,60 @@ SELECT LOWER('abcABC') FROM DUAL; -- 소문자로
 SELECT INITCAP('welcome to oracle') FROM DUAL; -- 어절단위로 나눠서 첫문자만 대문자 
 SELECT INITCAP('WELCOME TO ORACLE') FROM DUAL; -- 어절단위로 나눠서 첫문자만 대문자 
     -- EX. 이름이 SCOTT인 직원의 모든 필드
+    SELECT * FROM EMP WHERE UPPER(ENAME)='SCOTT';
+    SELECT * FROM EMP WHERE INITCAP(ENAME) = 'Scott';
+    SELECT * FROM EMP WHERE LOWER(ENAME) = 'scott';
+-- (2) 문자 연결(CONCAT함수, ||연산자)
+SELECT 'AB'||'CD'||'EF'||'GH' FROM DUAL;
+SELECT CONCAT(CONCAT('AB','CD'), CONCAT('EF','GH')) FROM DUAL;
+    -- "SMITH는 MANAGER다" 포맷으로 EMP테이블 데이터를 출력
+    SELECT ENAME || '는 ' || JOB || '다' FROM EMP;
+    SELECT CONCAT( CONCAT(ENAME, '는 '), CONCAT(JOB, '다') ) FROM EMP;
 
-
-
-
-
+-- (3)SUBSTR(STR, 시작위치, 문자갯수) : STR을 시작위치부터 문자갯수만큼 문자 추출
+--   SUBSTRB(STR, 시작바이트위치, 바이트갯수) : STR을 시작부터 바이트갯수만큼 문자 추출
+                -- O R A C L E
+                -- 1 2 3 4 5 6
+                ---6-5-7-3-2-1
+SELECT SUBSTR('ORACLE', 1, 1) FROM DUAL;
+SELECT SUBSTRB('ORACLE', 4, 2) FROM DUAL;
+SELECT SUBSTR('데이터베이스', 4,2) FROM DUAL;
+SELECT SUBSTRB('데이터베이스',4, 3) FROM DUAL;
+    -- 9월에 입사한 사원의 모든 필드 (날짜형 표기 : 'RR/MM/DD')
+    SELECT * FROM EMP WHERE SUBSTR(HIREDATE, 4, 2) = '09'; -- DATE(날짜)형도 가능
+    DESC EMP;
+    -- 9일 입사한 사원의 모든 필드
+    SELECT * FROM EMP WHERE SUBSTR(HIREDATE, -2, 2) = '09';
+    
+-- (4) LENGTH(STR) : STR의 글자수
+   -- LENGTHB(STR) : STR의 바이트 수
+SELECT LENGTH('ABCD') FROM DUAL; -- 글자수
+SELECT LENGTHB('ABCD') FROM DUAL; -- 저장될 바이트 수
+SELECT LENGTH('오라클'), LENGTHB('오라클') FROM DUAL;
+-- (5) LPAD(STR, 자리수, 채울문자):STR을 자리수만큼 확보하고 왼쪽 빈자리에 채울문자로 출력
+    -- LPAD(STR, 자리수) : STR을 자리수만큼 확보하고 왼쪽 빈자리에 ' '로 출력
+    -- RPAD(STR, 자리수, 채울문자):STR을 자리수만큼 확보하고 오른쪽 빈자리에 채울문자로 출력
+    -- RPAD(STR, 자리수) : STR을 자리수만큼 확보하고 오른쪽 빈자리에 ' '로 출력
+SELECT LPAD('ORACLE', 10, '#') FROM DUAL;
+SELECT LPAD('ORACLE', 10) FROM DUAL;
+DESC EMP;
+    -- ENAME : 최장 10글자, SAL : 5자리.2자리
+SELECT LPAD(ENAME, 10, '-'), LPAD(SAL,5,'$') FROM EMP; 
+    -- EX. 사번, 이름 (7369 S**** / 1111 W*** / 2222 M***** ) - RPAD, SUBSTR, LENGTH
+    SELECT EMPNO,
+            RPAD(SUBSTR(ENAME, 1, 1), LENGTH(ENAME), '*') ENAME
+        FROM EMP;
+    -- EX. 사번, 이름, 직책(9자리확보), 입사일
+    --    7369 Smith ____****K 80/12/**
+    --    7566 Jones ___*****R 81/02/**
+    SELECT EMPNO,
+            INITCAP(ENAME) ENAME,
+            LPAD(LPAD(SUBSTR(JOB, -1, 1), LENGTH(JOB), '*'), 9) JOB,
+            SUBSTR(HIREDATE, 1, 6) || '**'
+        FROM EMP;
+    SELECT EMPNO,
+            INITCAP(ENAME) ENAME,
+            LPAD(LPAD(SUBSTR(JOB, -1, 1), LENGTH(JOB), '*'), 9) JOB,
+            TO_CHAR(HIREDATE, 'RR/MM/') || '**'
+        FROM EMP;
 
