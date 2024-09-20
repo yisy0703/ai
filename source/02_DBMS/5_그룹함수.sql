76512514e785fafdbdbcb1e2d4d35451f56d7294
yisy0703 <yisy0703@naver.com>
2024-09-20 09:46:19 +0900

    Oracle 그룹함수

--- source/02_DBMS/5_그룹함수.sql
+++ source/02_DBMS/5_그룹함수.sql
@@ -14,7 +14,21 @@ SELECT ROUND(AVG(SAL),1), SUM(SAL), MIN(SAL), MAX(SAL),
         ROUND(VARIANCE(SAL),1), ROUND(STDDEV(SAL), 1), COUNT(SAL)
     FROM EMP;
     -- EX. 그룹함수 MIN, MAX, COUNT는 숫자형, 문자형, 날짜형 모두 가능
+    SELECT 
+        MIN(HIREDATE) "최초입사일", MAX(HIREDATE) "최근입사일",
+        MIN(ENAME), COUNT(JOB)
+        FROM EMP;
+    -- 탄탄EX1. 입사한지 가장 오래된 사원의 입사일과 가장 최근에 입사일을 출력
+    SELECT MIN(HIREDATE) FIRST, MAX(HIREDATE) LAST FROM EMP;
+    -- 탄탄EX2. 결과)FIRST                LAST
+    --              80/12/17:18,123일째  83/01/12:16,123일째
+    -- 탄탄EX3. 결과) 80년12월17일최초입사:18,123일째  83년01년12일최근입사:16,123일째
+    -- 탄탄EX4. 10번부서 소속의 사원 중에 COMM을 받는 사원의 수(COUNT함수 사용)
     
+
+
+
+
 
 
 
