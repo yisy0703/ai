52f0e3fd1eb8019fb343fc06b98910580e8d312d
yisy0703 <yisy0703@naver.com>
2024-09-20 10:54:19 +0900

    Oracle 그룹함수

--- source/02_DBMS/5_그룹함수.sql
+++ source/02_DBMS/5_그룹함수.sql
@@ -41,9 +41,37 @@ SELECT ROUND(AVG(SAL),1), SUM(SAL), MIN(SAL), MAX(SAL),
     SELECT COUNT(*) FROM EMP WHERE DEPTNO=10 AND COMM>0; -- 추천
     SELECT COUNT(COMM) FROM EMP WHERE DEPTNO=10; -- COMM이 0인 행을 포함
 
+-- ★ 2. GROUP BY 절 : 그룹함수를 쓰되, 어떤 컬럼 기준으로 그룹함수를 적용
+    -- EX. 부서번호별 최대급여, 급여평균, 급여표준편차 (부서번호순 정렬)
+    SELECT DEPTNO NO, MAX(SAL), AVG(SAL), STDDEV(SAL) 
+        FROM EMP 
+        GROUP BY DEPTNO -- GROUP BY 절은 필드의 별칭 사용 불가
+        ORDER BY NO;    -- ORDER BY 절은 필드의 별칭 사용 가능
+    -- 부서명별 최대급여, 평균급여 (평균급여순 정렬)
+    SELECT DNAME, MAX(SAL), AVG(SAL) AVGSAL
+        FROM EMP E, DEPT D
+        WHERE E.DEPTNO=D.DEPTNO
+        GROUP BY DNAME
+        ORDER BY AVG(SAL);
+        -- ORDER BY AVGSAL;
+    -- SAL이 5000미만인 사원에 대해서만 부서번호별 사원수, 최대급여, 최소급여, 평균급여
+        -- (부서번호 순 정렬)
+    SELECT DEPTNO, COUNT(*), MAX(SAL), MIN(SAL), AVG(SAL)
+        FROM EMP 
+        WHERE SAL<5000
+        GROUP BY DEPTNO
+        ORDER BY DEPTNO;
 
-
-
+-- ★ 3. HAVING 절 : 그룹함수의 결과를 제한할 때
+    -- (필드의 조건을 사용하여 제한할 때는 WHERE절)
+    -- EX.SAL이 5000미만인 사원에 대해서만 부서번호별 사원수, 최대급여, 최소급여, 평균급여
+        -- (급여평균이 1800이상만 출력하되 평균급여순 정렬)
+    SELECT DEPTNO, COUNT(*), MAX(SAL), MIN(SAL), AVG(SAL) AVGSAL
+        FROM EMP 
+        WHERE SAL<5000         
+        GROUP BY DEPTNO        -- GROUP BY 절에 필드 별칭 사용 불가
+        HAVING AVG(SAL)>=1800  -- HAVING절에 필드 별칭 사용 불가
+        ORDER BY AVGSAL;
 
 
 
