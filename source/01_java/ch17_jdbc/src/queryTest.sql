9c8d96680e4b6877f7877acf9db25a50dfcbc193
spacenyi <spacenyi@naver.com>
2024-09-30 21:34:26 +0900

    jdbc

--- source/01_java/ch17_jdbc/src/queryTest.sql
+++ source/01_java/ch17_jdbc/src/queryTest.sql
@@ -1,8 +1,7 @@
--- com.lec.ex1selectAll
-SELECT * 
-  FROM EMP;
+-- ★ ★ ★ com.lec.ex1selectAll ★ ★ ★
+SELECT * FROM EMP;
   
--- com.lec.ex2selectWhere
+-- ★ ★ ★ com.lec.ex2selectWhere ★ ★ ★
 -- Ex1. 부서번호를 받아 부서정보를 출력
 SELECT * FROM DEPT WHERE DEPTNO=10;
 INSERT INTO DEPT VALUES (50, 'IT', '서울');
