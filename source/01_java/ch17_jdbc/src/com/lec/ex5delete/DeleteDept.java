a7c8b7f87e09e4580b2cb836bcb147bf03abce71
yisy0703 <yisy0703@naver.com>
2024-10-01 13:10:57 +0900

    jdbc

--- source/01_java/ch17_jdbc/src/com/lec/ex5delete/DeleteDept.java
+++ source/01_java/ch17_jdbc/src/com/lec/ex5delete/DeleteDept.java
@@ -1,5 +1,6 @@
 package com.lec.ex5delete;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
@@ -13,7 +14,14 @@ public class DeleteDept {
 		int deptno = scanner.nextInt();
 		String sql = "DELETE FROM DEPT WHERE DEPTNO="+deptno;
 		try {
-			
+			conn = DriverManager.getConnection(url, "scott", "tiger");
+			stmt = conn.createStatement();
+			int result = stmt.executeUpdate(sql);
+			if(result>0) {
+				System.out.println(deptno + "번 부서를 삭제하였습니다.");
+			}else {
+				System.out.println(deptno + "번 부서는 유효한 부서가 아니라 삭제할 수 없습니다");
+			}
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		} finally {
---
+++ source/01_java/ch17_jdbc/src/com/lec/ex6prepareStatement/Ex1_InsertDept.java
@@ -0,0 +1,58 @@
+package com.lec.ex6prepareStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+public class Ex1_InsertDept {
+	public static void main(String[] args) {
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		Scanner scanner = new Scanner(System.in);
+		Connection conn = null;
+		//Statement  stmt = null;
+		PreparedStatement pstmt = null;
+		
+		System.out.print("입력할 부서번호는 >");
+		int deptno = scanner.nextInt();
+		System.out.print("입력할 부서명은 >");
+		String dname = scanner.next();
+		System.out.print("입력할 부서위치는 >");
+		String loc = scanner.next();
+		String sql = "INSERT INTO DEPT VALUES (?, UPPER(?), UPPER(?))";
+		try {
+			conn = DriverManager.getConnection(url, "scott", "tiger");
+			//stmt = conn.createStatement();
+			//stmt.executeUpdate(sql);
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, deptno); // 첫번째 물음표는 int값 deptno로 채움
+			pstmt.setString(2, dname);
+			pstmt.setString(3, loc);
+			int result = pstmt.executeUpdate();
+			System.out.println(result>0? "입력성공":"입력실패");
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}finally {
+			try { // 7. 연결해제
+				if(pstmt!=null) pstmt.close();
+				if(conn!=null) conn.close();
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
