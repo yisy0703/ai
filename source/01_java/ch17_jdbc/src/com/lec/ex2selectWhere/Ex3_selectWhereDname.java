26dc7df4fa0b2fbd243803d97286d3fd68ac57c7
yisy0703 <yisy0703@naver.com>
2024-10-01 10:54:47 +0900

    jdbc

--- source/01_java/ch17_jdbc/src/com/lec/ex2selectWhere/Ex3_selectWhereDname.java
+++ source/01_java/ch17_jdbc/src/com/lec/ex2selectWhere/Ex3_selectWhereDname.java
@@ -1,4 +1,7 @@
 package com.lec.ex2selectWhere;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Scanner;
 // 부서이름을 입력받아 해당 부서 정보 출력
 public class Ex3_selectWhereDname {
@@ -7,5 +10,12 @@ public class Ex3_selectWhereDname {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("원하는 부서명은 ?");
 		String dname = scanner.next();
+		Connection conn = null;
+		Statement  stmt = null;
+		ResultSet  rs   = null;
 	}
 }
+
+
+
+
