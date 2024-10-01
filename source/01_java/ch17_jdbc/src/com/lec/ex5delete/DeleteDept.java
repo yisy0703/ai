40be826963487c9f11765ce9fed605bb59dc4038
yisy0703 <yisy0703@naver.com>
2024-10-01 12:51:17 +0900

    jdbc

--- source/01_java/ch17_jdbc/src/com/lec/ex5delete/DeleteDept.java
+++ source/01_java/ch17_jdbc/src/com/lec/ex5delete/DeleteDept.java
@@ -1,8 +1,42 @@
 package com.lec.ex5delete;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 public class DeleteDept {
 	public static void main(String[] args) {
 		String url = "jdbc:oracle:thin:@localhost:1521:xe";
 		Scanner scanner = new Scanner(System.in);
+		Connection conn = null;
+		Statement  stmt = null;
+		System.out.print("삭제할 부서번호는 >");
+		int deptno = scanner.nextInt();
+		String sql = "DELETE FROM DEPT WHERE DEPTNO="+deptno;
+		try {
+			
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			try { // 7. 연결해제
+				if(stmt!=null) stmt.close();
+				if(conn!=null) conn.close();
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}// try-catch-finally
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
