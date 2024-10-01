347736370203ebdeca2101f2c987e29a140df401
yisy0703 <yisy0703@naver.com>
2024-10-01 11:04:00 +0900

    jdbc

--- source/01_java/ch17_jdbc/src/com/lec/ex2selectWhere/Ex3_selectWhereDname.java
+++ source/01_java/ch17_jdbc/src/com/lec/ex2selectWhere/Ex3_selectWhereDname.java
@@ -1,6 +1,8 @@
 package com.lec.ex2selectWhere;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
 // 부서이름을 입력받아 해당 부서 정보 출력
@@ -13,8 +15,38 @@ public class Ex3_selectWhereDname {
 		Connection conn = null;
 		Statement  stmt = null;
 		ResultSet  rs   = null;
+		String sql = "SELECT * FROM DEPT WHERE DNAME=UPPER('" + dname + "')";
+		sql = String.format("SELECT * FROM DEPT WHERE DNAME=UPPER('%s')", dname);
+		try {
+			conn = DriverManager.getConnection(url, "scott", "tiger");
+			stmt = conn.createStatement();
+			rs   = stmt.executeQuery(sql);
+			if(rs.next()) {
+				System.out.println("부서번호 : " + rs.getInt("deptno"));
+				System.out.println("부서이름 : " + rs.getString("dname"));
+				System.out.println("부서위치 : " + rs.getString("loc"));
+			}else {
+				System.out.println("해당 부서이름이 존재하지 않습니다");
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}finally {
+			try { // 7. 연결해제
+				if(rs!=null) rs.close();
+				if(stmt!=null) stmt.close();
+				if(conn!=null) conn.close();
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
 	}
 }
+
+
+
+
+
+
 
 
 
