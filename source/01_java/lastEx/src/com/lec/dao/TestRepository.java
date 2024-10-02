264c554030ac1adeaf8ed94c8749f57350933c84
yisy0703 <yisy0703@naver.com>
2024-10-02 16:27:04 +0900

    jdbc lastEx

--- source/01_java/lastEx/src/com/lec/dao/TestRepository.java
+++ source/01_java/lastEx/src/com/lec/dao/TestRepository.java
@@ -1,9 +1,25 @@
 package com.lec.dao;
+import java.util.ArrayList;
 
+import com.lec.dto.Person;
+// 내가 만든 DAO 메소드들에 오류가 있는지 테스트 해 봄
 public class TestRepository {
 	public static void main(String[] args) {
 		PersonRepository repository = PersonRepository.getInstance();
 		int result = repository.insertPerson("신길동", "가수", 70, 80, 90);
-		System.out.println();
+		System.out.println(result==PersonRepository.SUCCESS? "성공":"실패");
+		System.out.println("2.");
+		ArrayList<Person> person = repository.getPerson("배우");
+		for(Person p : person) {
+			System.out.println(p);
+		}
+		System.out.println("3.");
+		person = repository.getPerson();
+		for(Person p : person) {
+			System.out.println(p);
+		}
+		System.out.println("jnames");
+		ArrayList<String> jnames = repository.jnameList();
+			System.out.println(jnames);
 	}
 }
