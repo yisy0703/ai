9b2c05f1b28199e5da5ddfce6bad0d387c654838
yisy0703 <yisy0703@naver.com>
2024-10-02 14:29:30 +0900

    jdbc lastEx

---
+++ source/01_java/lastEx/db/1_DDL_dummyData.sql
@@ -0,0 +1,10 @@
+-- DDL & dummy Data insert
+-- 테이블 및 시퀀스 삭제
+DROP TABLE PERSON;
+DROP TABLE JOB;
+DROP SEQUENCE PERSON_PNO_SQ;
+-- 테이블 및 시퀀스 생성
+
+-- DUMMY DATA
+-- JOB (10, 배우) (20, 가수) (30, 엠씨)
+-- PERSON (3명정도)
---
+++ source/01_java/lastEx/db/2_DML_DAO.sql
@@ -0,0 +1,8 @@
+-- DAO에 쓸 QUERY
+-- 1번기능과 2번 기능에서 입력가능한 직업명들
+
+-- 1번 기능 : PNAME, JNAME, KOR, ENG, MAT를 입력받아 PERSON 테이블 INSERT
+
+-- 2번 기능 : JNAME을 입력받아 RANK, PNAME(PNO), JNAME, KOR, ENG, MAT, SUM 출력
+              -- RANK는 SUM 내림차순으로 순위를 정합니다
+-- 3번 기능 : RANK, PNAME(PNO), JNAME, KOR, ENG, MAT, SUM 출력
