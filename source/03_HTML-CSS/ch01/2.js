6ba0dee56fa1689a477fb253f5c85c5172d364f8
yisy0703 <yisy0703@naver.com>
2024-10-04 12:50:13 +0900

    웹표준HTML

---
+++ source/03_HTML-CSS/ch01/2.js
@@ -0,0 +1,7 @@
+﻿// 2.js
+// 다른이름으로 저장(utf-8인코딩방법으로 저장)
+/* 동적인 부분(JavaScript) */
+name = prompt("이름은 ?"); // 취소를 클릭하면 'null'리턴
+if (name != "null" && name != "") {
+    document.write(name + "님 반갑습니다<br>");
+}
