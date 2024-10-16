b1bd97deb1f0618cacd21ef3962b26dd780b67d5
yisy0703 <yisy0703@naver.com>
2024-10-16 17:13:37 +0900

    JavaScript 함수

--- source/04_javaScript/ch05_함수/sumAll.js
+++ source/04_javaScript/ch05_함수/sumAll.js
@@ -1,5 +1,16 @@
 function sumAll(){
   // 매개변수가 0개 : -999 return
   // 매개변수가 1개이상 : 매개변수들 누적합 return
-  
+  var result = 0;
+  if(arguments.length == 0){
+    result = -999;
+  }else if(arguments.length >= 1 ){
+    for(let idx = 0 ; idx<arguments.length ; idx++){
+      result += arguments[idx]; //result = result + arguments[idx];
+    }
+  }
+  return result;
 }
+console.log("sumAll() = ", sumAll());
+console.log("sumAll(1, 2, 3, 4, 5, 6) = ", sumAll(1, 2, 3, 4, 5, 6));
+console.log("sumAll(10, 20) = ", sumAll(10, 20));
