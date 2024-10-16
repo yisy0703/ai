ecb1f6bc24b783425da8a47e32df69d14091ec32
yisy0703 <yisy0703@naver.com>
2024-10-16 14:40:36 +0900

    JavaScript 함수

--- source/04_javaScript/ch05_함수/01_익명함수_화살표함수문법.js
+++ source/04_javaScript/ch05_함수/01_익명함수_화살표함수문법.js
@@ -16,3 +16,15 @@ funVar = i => {
 funVar(10);
 funVar = i => console.log('4. 매개변수 1개이고, 구현이 1줄인 화살표 함수 호출 : ' + i);
 funVar(20);
+funVar = (i, j) => console.log('5. 매개변수 2개 이상 함수 호출') ;
+funVar(10,20);
+funVar = function(i, j){
+  return i*10+j;
+}
+funVar = (i, j) => i*10 + j;  // 위 함수표현식과 동일
+console.log('6. return이 있는 화살표 함수 호출 : ' + funVar(1, 2));
+funVar = function(x){
+  return x*x;
+};
+funVar = x => x*x;
+console.log('7. 매개변수 1개, return이 있는 화살표 함수 호출 : ' + funVar(5));
