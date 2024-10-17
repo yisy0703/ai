b9d6655381b3ba7f8558dbba2cdb0159cfef9dbe
yisy0703 <yisy0703@naver.com>
2024-10-17 15:00:42 +0900

    JavaScript 함수

---
+++ source/04_javaScript/ch06_객체/4_replaceAll.js
@@ -0,0 +1,16 @@
+// replaceAll('안녕00방가00하이', '00', 'x') : 모든 00을 x로 교체하여 "안녕x방가x하이"를 return
+
+const replaceAll = (str, oldStr, newStr)=>{
+  // var splitStr = str.split(oldStr);
+  // var result = splitStr.join(newStr);
+  // return result;
+  return str.split(oldStr).join(newStr);
+};
+console.log(replaceAll('테스트<br>합니다<br>진짜로', '<br>', 'x'));
+console.log(replaceAll('테스트합니다 진짜로 테스트', ' ', ''));
+// var str = '안녕00방가00하이';
+// console.log('원본', str);
+// var splitStr = str.split('00');
+// console.log(splitStr);
+// var result = splitStr.join('x');
+// console.log(result);
