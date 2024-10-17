f2af1ca6f563c76cd3a2bf29cd34fa2093cca58b
yisy0703 <yisy0703@naver.com>
2024-10-17 18:02:52 +0900

    JavaScript 함수

---
+++ source/04_javaScript/ch07_생성자함수/3_class.js
@@ -0,0 +1,17 @@
+// class : ECMA script 6
+// caniuse.com를 이용해서 지원 브라우저를 확인
+class Person{
+  constructor(name, first, second){//생성자 함수 이름은 무조건 constructor
+    console.log('생성자 함수 호출');
+    this.name = name;
+    this.first = first;
+    this.second = second;
+  }
+  sum(){
+    return this.first + this.second;
+  }//sum
+}//class
+var hong = new Person('홍길동', 90, 97);//{'name':'홍길동', 'first':90, 'second':97, 'sum':fu~}
+console.log(hong.name, ',', hong.first, ',', hong.second);
+console.log('합계 :' +hong.sum());
+console.log(hong);
