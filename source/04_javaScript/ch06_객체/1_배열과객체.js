b5b06b01e6f7bee9f50a2b41150ce9775292b718
yisy0703 <yisy0703@naver.com>
2024-10-17 12:44:55 +0900

    JavaScript 함수

---
+++ source/04_javaScript/ch06_객체/1_배열과객체.js
@@ -0,0 +1,15 @@
+/*
+class Person {
+  private String name;
+  private int age
+}
+Person person = new Person("홍길동", 22);
+*/
+const person = {name:'홍길동', age:22};
+console.log('name:', person.name, ', age:' + person.age);
+person.name = '신길동';
+console.log('name:', person['name'], ', age:' + person['age']);
+/***************************************************** */
+const arr = ['홍길동', 22]; // arr={0:'홍길동', 1:22}
+arr[0] = '신길동';
+console.log(arr[0], arr[0])
