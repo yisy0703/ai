42cae9c4a9f48594e409aed58364235b0c0faa7d
yisy0703 <yisy0703@naver.com>
2024-10-18 10:18:37 +0900

    JavaScript 기본객체

--- source/04_javaScript/ch07_생성자함수/3_class.js
+++ source/04_javaScript/ch07_생성자함수/3_class.js
@@ -1,6 +1,6 @@
 // class : ECMA script 6
 // caniuse.com를 이용해서 지원 브라우저를 확인
-class Person{
+class Person {
   constructor(name, first, second){//생성자 함수 이름은 무조건 constructor
     console.log('생성자 함수 호출');
     this.name = name;
