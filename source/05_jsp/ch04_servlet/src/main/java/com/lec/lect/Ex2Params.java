07052055d752a089b5a0d3e0f08e465dc9b2dcb2
yisy0703 <yisy0703@naver.com>
2024-10-23 13:13:17 +0900

    jsp서블릿

---
+++ source/05_jsp/ch04_servlet/src/main/java/com/lec/lect/Ex2Params.java
@@ -0,0 +1,41 @@
+package com.lec.lect;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+@WebServlet("/ex2")
+public class Ex2Params extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doGet()호출");
+		actionDo(request, response);
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doPost()호출");
+		actionDo(request, response);
+	}
+	private void actionDo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		// 복수개의 menu, rest파라미터, 1개의 gender 파라미터
+		String gender = request.getParameter("gender"); // 같은 이름의 파라미터가 없을 때
+		String[] menu = request.getParameterValues("menu"); // 같은 이름의 파라미터가 전달될 때
+		String[] rest = request.getParameterValues("rest");
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter(); // 스트림 생성
+		out.println("<h2>선택한 메뉴는 " + Arrays.toString(menu) + "입니다</h2>");
+		out.println("<h2>선택한 식당은 " + Arrays.toString(rest) + "입니다</h2>");
+		out.println("<h2>성별은 " + gender +"입니다<h2>");
+		out.close();
+	}
+}
+
+
+
+
+
+
+
+
