9d6a6c4fe1f006663c25a530f90c16b97f227d30
yisy0703 <yisy0703@naver.com>
2024-10-25 12:44:16 +0900

    DB연동웹프로그래밍

--- source/05_jsp/ch06_db/src/main/java/com/lec/dao/DeptRepository.java
+++ source/05_jsp/ch06_db/src/main/java/com/lec/dao/DeptRepository.java
@@ -1,4 +1,14 @@
 package com.lec.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.lec.dto.Dept;
+
 public class DeptRepository {
 	private String driver = "oracle.jdbc.driver.OracleDriver";
 	private String url    = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
@@ -11,9 +21,52 @@ public class DeptRepository {
 		}
 		return INSTANCE;
 	}	
-	private DeptRepository() {}
+	private DeptRepository() {
+		try {
+			Class.forName(driver); // 1단계
+		} catch (ClassNotFoundException e) {
+			System.out.println(e.getMessage());
+		}
+	}
 	//SELECT DEPTNO, DNAME FROM DEPT 실행결과 return 함수
+	public ArrayList<Dept> deptList(){
+		ArrayList<Dept> dtos = new ArrayList<Dept>();
+		Connection        conn  = null;
+		PreparedStatement pstmt = null;
+		ResultSet         rs    = null;
+		String query = "SELECT DEPTNO, DNAME FROM DEPT";
+		try {
+			conn = DriverManager.getConnection(url, uid, upw);// (2)
+			
+			
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			try {
+				
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+		return dtos;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
