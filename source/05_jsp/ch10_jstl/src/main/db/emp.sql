f24f2159219ed6064f1f62cc7396aaa2560ea3ea
yisy0703 <yisy0703@naver.com>
2024-10-29 18:10:34 +0900

    jstl 태그라이브러리

--- source/05_jsp/ch10_jstl/src/main/db/emp.sql
+++ source/05_jsp/ch10_jstl/src/main/db/emp.sql
@@ -3,4 +3,4 @@ SELECT * FROM EMP;
 -- quiz에 쓸 public ArrayList(Emp) getEmpList(String schName, String schJob)
 SELECT * FROM EMP
   WHERE ENAME LIKE '%'||UPPER(TRIM('  s  '))||'%'   
-    AND JOB LIKE '%'||TRIM(UPPER(' ')) ||'%';
+    AND JOB LIKE '%'||TRIM(UPPER(null)) ||'%';
