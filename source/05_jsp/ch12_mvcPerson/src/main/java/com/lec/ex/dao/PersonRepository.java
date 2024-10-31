b542b7009db5c79ab97a5ebe97b4d15f5fc1a937
yisy0703 <yisy0703@naver.com>
2024-10-31 13:11:49 +0900

    MVC패턴

--- source/05_jsp/ch12_mvcPerson/src/main/java/com/lec/ex/dao/PersonRepository.java
+++ source/05_jsp/ch12_mvcPerson/src/main/java/com/lec/ex/dao/PersonRepository.java
@@ -74,10 +74,14 @@ public class PersonRepository {
 		Connection        conn  = null;
 		PreparedStatement pstmt = null;
 		ResultSet         rs    = null;
-		String sql = "";
+		String sql = "SELECT * "
+				+ "  FROM (SELECT ROWNUM RN, A.* FROM (SELECT * FROM PERSON ORDER BY ID DESC) A)"
+				+ "  WHERE RN BETWEEN ? AND ?";
 		try {
 			conn = ds.getConnection();
 			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, startRow);
+			pstmt.setInt(2, endRow);
 			rs    = pstmt.executeQuery();
 			while(rs.next()) {
 				int id    	   = rs.getInt("id");
@@ -96,11 +100,128 @@ public class PersonRepository {
 		}
 		return dtos;
 	}
-	//2. getCount (PAGING처리시 필요한 전체 갯수) : public int getCount()
-	//3. 추가 : public int insertPerson(Person person)
-	//4. 한명출력 : public Person getPerson(int id)
-	//5. 수정 : public int updatePerson(Person person)
-	//6. 삭제 : public int deletePerson(int id)
+	//2. getCount (PAGING처리시 필요한 전체 갯수)
+	public int getCount() {
+		int count = 0;
+		Connection        conn  = null;
+		PreparedStatement pstmt = null;
+		ResultSet         rs    = null;
+		String sql = "SELECT COUNT(*) FROM PERSON";
+		try {
+			conn = ds.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			rs    = pstmt.executeQuery();
+			rs.next();
+			count = rs.getInt(1);
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			close(rs, pstmt, conn);
+		}
+		return count;
+	}
+	//3. 추가
+	public int insertPerson(Person person) {
+		int result = FAIL;
+		Connection        conn  = null;
+		PreparedStatement pstmt = null;
+		String sql = "INSERT INTO PERSON (ID, NAME, TEL, ADDRESS, BIRTH, MEMO)"
+				+ "  VALUES (PERSON_ID_SEQ.NEXTVAL, ?, ?, ?, ?, ?)";
+		try {
+			conn = ds.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, person.getName());
+			pstmt.setString(2, person.getTel());
+			pstmt.setString(3, person.getAddress());
+			pstmt.setDate(4, person.getBirth());
+			pstmt.setString(5, person.getMemo());
+			pstmt.executeUpdate();
+			result = SUCCESS;
+			System.out.println("추가 성공");
+		} catch (SQLException e) {
+			System.out.println(e.getMessage() + person + "추가 실패");
+		} finally {
+			close(null, pstmt, conn);
+		}
+		return result;
+	}
+	//4. 한명출력 : 
+	public Person getPerson(int id) {
+		Person dto = null;
+		Connection        conn  = null;
+		PreparedStatement pstmt = null;
+		ResultSet         rs    = null;
+		String sql = "SELECT * FROM PERSON WHERE ID=?";
+		try {
+			conn = ds.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, id);
+			rs    = pstmt.executeQuery();
+			if(rs.next()) {
+				String name    = rs.getString("name");
+				String tel     = rs.getString("tel");
+				String address = rs.getString("address");
+				Date birth     = rs.getDate("birth");
+				String memo    = rs.getString("memo");
+				Timestamp rdate= rs.getTimestamp("rdate");
+				dto = new Person(id, name, tel, address, birth, memo, rdate);
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			close(rs, pstmt, conn);
+		}
+		return dto;
+	}
+	//5. 수정
+	public int updatePerson(Person person) {
+		int result = FAIL;
+		Connection        conn  = null;
+		PreparedStatement pstmt = null;
+		String sql = "UPDATE PERSON "
+				+ "  SET NAME = ?,"
+				+ "    TEL = ?,"
+				+ "    ADDRESS = ?,"
+				+ "    BIRTH = ?,"
+				+ "    MEMO = ?"
+				+ "  WHERE ID = ?";
+		try {
+			conn = ds.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, person.getName());
+			pstmt.setString(2, person.getTel());
+			pstmt.setString(3, person.getAddress());
+			pstmt.setDate(4, person.getBirth());
+			pstmt.setString(5, person.getMemo());
+			pstmt.setInt(6, person.getId());
+			result = pstmt.executeUpdate();
+			System.out.println(result==SUCCESS ? "수정성공":"수정실패");
+		} catch (SQLException e) {
+			System.out.println(e.getMessage() + person + "제약조건에 맞지 않아 오류");
+		} finally {
+			close(null, pstmt, conn);
+		}
+		return result;
+	}
+	//6. 삭제
+	public int deletePerson(int id) {
+		int result = FAIL;
+		Connection        conn  = null;
+		PreparedStatement pstmt = null;
+		String sql = "DELETE FROM PERSON WHERE ID = ?";
+		try {
+			conn = ds.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, id);
+			result = pstmt.executeUpdate();
+			System.out.println(result==SUCCESS ? "삭제성공":"삭제실패");
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			close(null, pstmt, conn);
+		}
+		return result;
+	}
 	private void close(ResultSet rs, PreparedStatement pstmt, Connection conn) {
 		try {
 			if(rs!=null) rs.close();
---
+++ source/05_jsp/ch12_mvcPerson/src/main/java/com/lec/ex/dao/PersonRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.lec.ex.dao;
+import java.io.IOException;
+import java.util.ArrayList;
+import com.lec.ex.dto.Person;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+@WebServlet("/PersonDaoTest")
+public class PersonRepositoryTest extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PersonRepository dao = PersonRepository.getInstance();
+		ArrayList<Person> personList = dao.getPersonList();
+		System.out.println("1. list(paging X)");
+		for(Person person : personList) {
+			System.out.println(person);
+		}
+		System.out.println("1. list(2등~3등)");
+		personList = dao.getPersonList(2, 3);
+		for(Person person : personList) {
+			System.out.println(person);
+		}
+		System.out.println("3. getCount");
+		System.out.println("전체 갯수 : " + dao.getCount());
+		System.out.println("4. 한명 출력 ");
+		System.out.println("id가 1인 사람 : " + dao.getPerson(1));
+		Person newPerson = new Person(0, "김수한", null, "무안", null, "memo", null);
+		int result = dao.insertPerson(newPerson);
+		if(result == PersonRepository.SUCCESS) {
+			System.out.println(newPerson.getName() + " 추가 성공");
+		}
+	}
+}
