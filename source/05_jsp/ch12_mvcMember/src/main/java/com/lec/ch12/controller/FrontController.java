9d4df7e925085326c79207700a3516e5d820f5d1
yisy0703 <yisy0703@naver.com>
2024-10-31 10:37:11 +0900

    MVC패턴

--- source/05_jsp/ch12_mvcMember/src/main/java/com/lec/ch12/controller/FrontController.java
+++ source/05_jsp/ch12_mvcMember/src/main/java/com/lec/ch12/controller/FrontController.java
@@ -26,18 +26,26 @@ public class FrontController extends HttpServlet {
 			service = new MemberListService();
 			service.execute(request, response);
 			viewPage = "member/memberList.jsp";
-		}else if(command.equals("/memberJoinView.do")) {
-			viewPage = "member/memberJoin.jsp";
 		}else if(command.equals("/memberJoin.do")) {
+			viewPage = "member/memberJoin.jsp";
+		}
+		RequestDispatcher dispatcher = request.getRequestDispatcher(viewPage);
+		dispatcher.forward(request, response);
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 들어온 요청을 판별하고, 그에 따른 service.execute(req, res) 호출한후 view단으로 
+		String uri = request.getRequestURI(); // "/ch12_mvcMember/memberList.do"
+		String conPath = request.getContextPath(); // "/ch12_mvcMember"
+		String command = uri.substring(conPath.length()); // "/memberList.do"
+		Service service = null;
+		String viewPage = null;
+		if(command.equals("/memberJoin.do")) {
 			service = new MemberJoinService();
 			service.execute(request, response);	
 			viewPage = "memberList.do";
 		}
 		RequestDispatcher dispatcher = request.getRequestDispatcher(viewPage);
 		dispatcher.forward(request, response);
-	}
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
 	}
 }
 
