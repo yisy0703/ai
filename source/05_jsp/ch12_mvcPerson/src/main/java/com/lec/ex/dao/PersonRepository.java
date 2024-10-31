af4e4c6a211ca4e1aa673c307167bed903797e8e
yisy0703 <yisy0703@naver.com>
2024-10-31 12:39:07 +0900

    MVC패턴

--- source/05_jsp/ch12_mvcPerson/src/main/java/com/lec/ex/dao/PersonRepository.java
+++ source/05_jsp/ch12_mvcPerson/src/main/java/com/lec/ex/dao/PersonRepository.java
@@ -1,11 +1,18 @@
 package com.lec.ex.dao;
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
+
+import com.lec.ex.dto.Person;
 public class PersonRepository {
 	public static final int SUCCESS = 1;
 	public static final int FAIL    = 0;
@@ -33,13 +40,84 @@ public class PersonRepository {
 //		}
 //		return conn;
 //	}
-	//1. LIST (PAGING없이)public ArrayList<Person> getPersonList()
-	//1. LIST (PAGING추가) : public ArrayList<Person> getPersonList(int startRow, int endRow)
+	//1. LIST (PAGING없이)
+	public ArrayList<Person> getPersonList(){
+		ArrayList<Person> dtos = new ArrayList<Person>();
+		Connection        conn  = null;
+		PreparedStatement pstmt = null;
+		ResultSet         rs    = null;
+		String sql = "SELECT * FROM PERSON";
+		try {
+			conn = ds.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			rs    = pstmt.executeQuery();
+			while(rs.next()) {
+				int id    	   = rs.getInt("id");
+				String name    = rs.getString("name");
+				String tel     = rs.getString("tel");
+				String address = rs.getString("address");
+				Date birth     = rs.getDate("birth");
+				String memo    = rs.getString("memo");
+				Timestamp rdate= rs.getTimestamp("rdate");
+				dtos.add(new Person(id, name, tel, address, birth, memo, rdate));
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			close(rs, pstmt, conn);
+		}
+		return dtos;
+	}
+	//1. LIST (PAGING추가) 
+	public ArrayList<Person> getPersonList(int startRow, int endRow){
+		ArrayList<Person> dtos = new ArrayList<Person>();
+		Connection        conn  = null;
+		PreparedStatement pstmt = null;
+		ResultSet         rs    = null;
+		String sql = "";
+		try {
+			conn = ds.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			rs    = pstmt.executeQuery();
+			while(rs.next()) {
+				int id    	   = rs.getInt("id");
+				String name    = rs.getString("name");
+				String tel     = rs.getString("tel");
+				String address = rs.getString("address");
+				Date birth     = rs.getDate("birth");
+				String memo    = rs.getString("memo");
+				Timestamp rdate= rs.getTimestamp("rdate");
+				dtos.add(new Person(id, name, tel, address, birth, memo, rdate));
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			close(rs, pstmt, conn);
+		}
+		return dtos;
+	}
 	//2. getCount (PAGING처리시 필요한 전체 갯수) : public int getCount()
 	//3. 추가 : public int insertPerson(Person person)
 	//4. 한명출력 : public Person getPerson(int id)
 	//5. 수정 : public int updatePerson(Person person)
 	//6. 삭제 : public int deletePerson(int id)
+	private void close(ResultSet rs, PreparedStatement pstmt, Connection conn) {
+		try {
+			if(rs!=null) rs.close();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		try {
+			if(pstmt!=null) pstmt.close();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		try {
+			if(conn!=null) conn.close();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+	}
 }
 
 
