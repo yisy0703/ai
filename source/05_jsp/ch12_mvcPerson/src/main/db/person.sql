0edaede90dd5de3169056f7d6724fba75b2418e0
yisy0703 <yisy0703@naver.com>
2024-10-31 11:55:22 +0900

    MVC패턴

--- source/05_jsp/ch12_mvcPerson/src/main/db/person.sql
+++ source/05_jsp/ch12_mvcPerson/src/main/db/person.sql
@@ -33,7 +33,7 @@ SELECT *
 SELECT COUNT(*) FROM PERSON;
 -- 3. 추가 : public int insertPerson(Person person)
 INSERT INTO PERSON (ID, NAME, TEL, ADDRESS, BIRTH, MEMO)
-  VALUES (PERSON_ID_SEQ.NEXTVAL, '최길뚱', '010-6666-6666', '부산', '02/01/01', '테스트');
+  VALUES (PERSON_ID_SEQ.NEXTVAL, '최길동', '010-6666-6666', '부산', '02/01/01', '테스트');
 -- 4. 한명출력 : public Person getPerson(int id)
 SELECT * FROM PERSON WHERE ID=1;
 -- 5. 수정 : public int updatePerson(Person person)
---
+++ source/05_jsp/ch12_mvcPerson/src/main/java/com/lec/ex/dto/PersonDao.java
@@ -0,0 +1,8 @@
+package com.lec.ex.dto;
+
+import javax.sql.DataSource;
+
+public class PersonDao {
+	public 
+	private DataSource ds;
+}
