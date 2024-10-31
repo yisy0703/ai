b342627ff8a3235f1e6c72c6e7e5052c063062c5
yisy0703 <yisy0703@naver.com>
2024-10-31 14:17:16 +0900

    MVC패턴

--- source/05_jsp/ch12_mvcPerson/src/main/java/com/lec/ex/dao/PersonRepositoryTest.java
+++ source/05_jsp/ch12_mvcPerson/src/main/java/com/lec/ex/dao/PersonRepositoryTest.java
@@ -25,19 +25,19 @@ public class PersonRepositoryTest extends HttpServlet{
 		System.out.println("3. getCount");
 		System.out.println("전체 갯수 : " + dao.getCount());
 		System.out.println("4. 한명 출력 ");
-		Person person = dao.getPerson(1);
+		Person person = dao.getPerson(1); // id가 1인 person 정보
 		System.out.println("id가 1인 사람 : " + person);
 		Person newPerson = new Person(0, "김수한", null, "무안", null, "memo", null);
 		int result = dao.insertPerson(newPerson);
 		if(result == PersonRepository.SUCCESS) {
 			System.out.println(newPerson.getName() + " 추가 성공");
 		}
-		person.setName("바꿨다");
-		person.setAddress("이사간데");
+		person.setName("이름싹");
+		person.setAddress("이사간 곳");
 		result = dao.updatePerson(person);
 		if(result == PersonRepository.SUCCESS) {
 			System.out.println(newPerson.getName() + " 수정 성공");
-			System.out.println("수정 한 후 :" + dao.getPerson(1));
+			System.out.println("id가 1인 person 수정한 후 :" + dao.getPerson(1));
 		}
 		
 	}
