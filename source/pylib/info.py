9c811845a53fb2f603a48e5fb03532346e382d7e
yisy0703 <yisy0703@naver.com>
2024-11-12 15:32:56 +0900

    python 모듈과패키지

---
+++ source/pylib/info.py
@@ -0,0 +1,39 @@
+'''
+Cal obj = new Cal(10, 20)
+class Cal{
+  private int num1;
+  private int num2;
+  public Cal(int num1, int num2){
+    this.num1 = num1;
+    this.num2 = num2;
+  }
+  public Cal(){
+    num1 = num2 = 1;
+  }
+  public int add(){
+    return num1+num2;
+  }
+  public int sub(){
+    return num1-num2;
+  }
+}
+'''
+class Cal:
+  def __init__(self, num1=1, num2=1):
+    self.num1 = num1
+    self.num2 = num2
+  def add(self):
+    return self.num1+self.num2
+  def sub(self):
+    return self.num1 - self.num2
+  
+def main():
+  print('Cal 객체 생성 test입니다')
+  obj = Cal(10, 20)
+  print('obj.num1 :', obj.num1)
+  print('obj.num2 :', obj.num2)
+  print('add test :', obj.add())
+  print('sub test :', obj.sub())
+
+if __name__=='__main__':
+  main()
---
+++ source/pylib/test.py
@@ -0,0 +1,16 @@
+'''
+class Cal{
+  private int num1;
+  private int num2;
+  public Cal(int num1, int num2){
+    this.num1 = num1;
+    this.num2 = num2;
+  }
+  public int add(){
+    return num1+num2;
+  }
+  public int sub(){
+    return num1-num2;
+  }
+}
+'''
