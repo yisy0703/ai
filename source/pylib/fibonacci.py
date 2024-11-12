9b35b1d0eaa47470f5538b5d0d54aaae1556c5bf
yisy0703 <yisy0703@naver.com>
2024-11-12 15:01:11 +0900

    python 모듈과패키지

--- source/pylib/fibonacci.py
+++ source/pylib/fibonacci.py
@@ -9,10 +9,26 @@ def fibo_print(n):
     a, b = b, a+b
   print() # 개행
 
+def fibo_return(n):
+  'n미만의 피보나치 수열의 list를 return'
+  a, b = 0, 1
+  result = []
+  while a<n:
+    result.append(a)
+    a, b = b, a+b
+  return result
+
+
 if __name__ == '__main__':
   import sys
   # sys.argv : 명령행 인수
   if len(sys.argv) > 1:
+    print('1. print test :', end=' ')
     fibo_print(int(sys.argv[1]))
+    lst = fibo_return(int(sys.argv[1]))
+    print('2. return된 list test :', lst)
   else:
-    fibo_print(100)
+    print('1. print test :', end=' ')
+    fibo_print(int(100))
+    lst = fibo_return(100)
+    print('2. return된 list test :', lst)
