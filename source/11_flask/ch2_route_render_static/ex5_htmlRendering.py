6665984535129601670590d41046c7131dffe13a
yisy0703 <yisy0703@naver.com>
2025-01-08 11:01:37 +0900

    flask redering

--- source/11_flask/ch2_route_render_static/ex5_htmlRendering.py
+++ source/11_flask/ch2_route_render_static/ex5_htmlRendering.py
@@ -6,5 +6,26 @@ app = Flask(__name__)
 def hello():
     return render_template("05_index.html")
 
+@app.route('/profile/<name>/<int:age>') #/profile/홍길동/33
+def get_profile(name, age):
+    return render_template("05_profile.html",
+                           name=name,
+                           age=age)
+
 if __name__=='__main__':
+    with app.test_request_context():
+        print('※', url_for('hello'))
+        print('※', url_for('get_profile', name='HONG', age=22))
     app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
