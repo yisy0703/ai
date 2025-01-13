d53cebd2e509ddcc7992b286a59db48a31a5bdf0
yisy0703 <yisy0703@naver.com>
2025-01-13 10:26:44 +0900

    플라스크 fileUpload

---
+++ source/11_flask/ch5_fileupload/step1_app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+import os
+# 업로드 폴더 경로 설정(폴더없으면 폴더생성)
+UPLOAD_FOLDER = './upload/'
+if not os.path.exists(UPLOAD_FOLDER):
+  os.makedirs(UPLOAD_FOLDER)
+
+app = Flask(__name__)
+# 파일 업로드 용량 제한(단위:바이트)
+# 용량 제한이 없을 경우, 서버에서 매우 큰 용량을 업로드 할 403 error경우
+# print(app.config)
+app.config['MAX_CONTENT_LENGTH'] = 1024*1024*10 # 최대 업로드 용량
+
+@app.route('/', methods=['get','post']) # 대소문자 가능
+def upload_file():
+  if request.method == 'POST': # 대문자만
+    '''
+    파라미터로 전달받은 파일을 upload 폴더에 저장하고 완료 페이지로 가기
+    '''
+    return render_template("check.html")
+  return render_template("upload.html")
+
+
+
+
+
+
+
+
+
+
+
+
+
