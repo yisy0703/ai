ad97cb5d2f2b2dfd14815856ce44ee588e29c50c
yisy0703 <yisy0703@naver.com>
2025-01-23 10:33:48 +0900

    FastAPI db

--- source/12_fastAPI/ch4_todo_db/src/database/repository.py
+++ source/12_fastAPI/ch4_todo_db/src/database/repository.py
@@ -1,6 +1,10 @@
-from connection import conn
+from typing import List
 
-def get_todos():
+from database.connection import conn
+from models import ToDoRequest
+
+
+def get_todos() -> List[dict]:
   cursor = conn.cursor()
   sql = 'SELECT * FROM TODO ORDER BY ID'
   cursor.execute(sql)
@@ -13,5 +17,54 @@ def get_todos():
                   'is_done':result[2]})
   return todos
 
+def get_todo(todo_id:int)->dict:
+  cursor = conn.cursor()
+  sql = 'SELECT * FROM TODO WHERE ID = :id';
+  cursor.execute(sql, {'id':todo_id})
+  result = cursor.fetchone() # 결과를 한행 튜플로 받음
+  cursor.close()
+  if result:
+    return {'id':result[0],
+            'contents':result[1],
+            'is_done':result[2]}
+  return {}
+
+def create_todo(todo:ToDoRequest)->str:
+  cursor = conn.cursor()
+  print('추가될 내용 :', todo.contents, todo.is_done)
+  sql = '''INSERT INTO TODO (ID, CONTENTS, IS_DONE) 
+            VALUES (TODO_SQ.NEXTVAL, :contents, :is_done)'''
+  cursor.execute(sql, {'contents':todo.contents,
+                       'is_done':todo.is_done})
+  conn.commit()
+  result = cursor.rowcount # sql문이 수행된 행수
+  cursor.close()
+  if result:
+    return '추가성공'
+  return '추가 실패'
+
+def update_todo(id:int, contents:str, is_done:bool)->str:
+
 if __name__=='__main__':
-  print(get_todos())
+  todo = ToDoRequest()
+  todo.contents = '추가할 todo'
+  todo.is_done = True
+  print('3. create :', create_todo(todo))
+  print('1. 목록 :', get_todos())
+  print('2. 상세 :', get_todo(1))
+  print('2. 상세 :', get_todo(9))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
