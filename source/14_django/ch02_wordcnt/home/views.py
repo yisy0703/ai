c7c962c223a68abbf873651dab87d961c537f69e
yisy0703 <yisy0703@naver.com>
2025-02-11 11:38:36 +0900

    dJango 앱

--- source/14_django/ch02_wordcnt/home/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def index(request):
-  context = {'msg': 'WordCount Welcome Page',
-             'greeting':'Hello, Django(장고)'}
-  return render(request=request,
-                template_name='home/index.html',
-                context=context)
