e2dbd94f4d63a570409a72579ba2882940831525
yisy0703 <yisy0703@naver.com>
2025-02-13 11:17:25 +0900

    django get|post諛⑹떇

--- source/14_django/ch02_wordcnt/wordcnt/views.py
+++ source/14_django/ch02_wordcnt/wordcnt/views.py
@@ -12,7 +12,9 @@ def about(request):
 def result(request):
   # post방식으로 전달된 fulltxt 파라미터 받아 글자수, 단어수, 단어당출현빈도 계산
   # print(request.POST['fulltxt'], request.POST.get('fulltxt', ''))
-  fulltxt = request.POST['fulltxt'] #홍 홍 화이팅
+  # fulltxt = request.POST['fulltxt'] #홍 홍 화이팅
+  fulltxt = request.GET['fulltxt']
+  # fulltxt = request.GET.get('fulltxt','')
   strlength = len(fulltxt) # 글자수
   words = fulltxt.split() # space단위로 단어 분리 ['홍','홍','화이팅']
   wordcnt = len(words)    # 단어 갯수
