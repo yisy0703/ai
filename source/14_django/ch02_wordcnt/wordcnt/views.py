c56627bae07ed3417715aa55b624464af8722f4c
yisy0703 <yisy0703@naver.com>
2025-02-13 10:50:14 +0900

    django get|post諛⑹떇

--- source/14_django/ch02_wordcnt/wordcnt/views.py
+++ source/14_django/ch02_wordcnt/wordcnt/views.py
@@ -16,8 +16,21 @@ def result(request):
   strlength = len(fulltxt) # 글자수
   words = fulltxt.split() # space단위로 단어 분리 ['홍','홍','화이팅']
   wordcnt = len(words)    # 단어 갯수
-  return HttpResponse('TEST')
-
+  words_dic = dict() # 빈 딕셔너리
+  for word in words:
+    if word in words_dic.keys():
+      words_dic[word] += 1 # 앞에서 출현했던 단어
+    else:
+      words_dic[word] = 1
+  context = {
+    'fulltxt'  : fulltxt,
+    'strlength': strlength,
+    'wordcnt'  : wordcnt,
+    'dict'     : words_dic.items()
+  }
+  return render(request,
+                'wordcnt/result.html',
+                context)
 
 
 
