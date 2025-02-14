4877a4342d9b6dfb7b6f71c63ad7e0d62518d365
yisy0703 <yisy0703@naver.com>
2025-02-14 11:44:57 +0900

    django orm기술

--- source/14_django/ch03_orm/ch03/urls.py
+++ source/14_django/ch03_orm/ch03/urls.py
@@ -16,7 +16,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from student import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', views.index, name='index'),
 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- source/14_django/ch03_orm/student/views.py
+++ source/14_django/ch03_orm/student/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from student.models import Student
 
 # Create your views here.
+def index(request):
+  students = Student.objects.all()
+  return render(request,
+                'student/list.html',
+                {'students':students})
