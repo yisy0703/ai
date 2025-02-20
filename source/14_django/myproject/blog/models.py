b2e7924a1ebf10d4f31083262c1af66610b7c9eb
yisy0703 <yisy0703@naver.com>
2025-02-20 11:23:10 +0900

    django 프로젝트

--- source/14_django/myproject/blog/models.py
+++ source/14_django/myproject/blog/models.py
@@ -1,11 +1,36 @@
 from django.db import models
+import re
+from django.forms import ValidationError
+REGION_CHOICE = (
+  ("Europe","유럽"),
+  ("Asia","아시아"),
+  ("Africa","아프리카"),
+  ("Oceania","오세아니아"),
+  ("America","아메리카"),
+)
+def lnglat_validator(value):
+  if not re.match(r'(\d+\.?\d*),(\d+\.?\d*)', value):
+    raise ValidationError('Invalid LngLat. ex:38,125')
 
 class Post(models.Model): # 테이블명 : blog_post
   # id = models.AutoField(primary_key=True) PK가 없을 경우 자동 생성
-  title = models.CharField(max_length=100) # 최대 길이 반드시 지정 VARCHAR 타입
-  content = models.TextField() # 최대 길이 제한이 없음 CLOB, TEXT 타입
+  title = models.CharField(verbose_name="제목",
+                           max_length=100) # 최대 길이 반드시 지정 VARCHAR 타입
+  content = models.TextField("본문") # 최대 길이 제한이 없음 CLOB, TEXT 타입
   create_at = models.DateField(auto_now_add=True)
   update_at = models.DateTimeField(auto_now=True)
+  region = models.CharField(verbose_name="지역",
+                            max_length=20,
+                            choices=REGION_CHOICE,
+                            default='Asia')
+  lnglat = models.CharField(verbose_name="경,위도",
+                            max_length=100,
+                            blank=True,
+                            null=True,
+                            help_text="경도,위도 포맷",
+                            validators=[lnglat_validator]) # 38.5,125.4125
+  url = models.URLField(blank=True, null=True)
+
   def __str__(self):
     return "제목:{} - {}작성, {:%Y-%m-%d %p %I:%M:%S} 최종수정".format(self.title,
                                                                self.create_at,
--- source/14_django/myproject/blog/tests.py
+++ source/14_django/myproject/blog/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
-
-# Create your tests here.
+import re
+# 38.5,125.4125   38,128
+value = input('경도,위도 ?')
+if not re.match(r'(\d+\.?\d*),(\d+\.?\d*)', value):
+  print('경도,위도 포맷으로 입력 안 했네')
