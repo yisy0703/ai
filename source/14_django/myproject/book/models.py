8b476465d569d9013e48e09901cb90a2f1704246
yisy0703 <yisy0703@naver.com>
2025-02-25 11:24:34 +0900

    django Form객체

--- source/14_django/myproject/book/models.py
+++ source/14_django/myproject/book/models.py
@@ -9,13 +9,17 @@ def min_length_3_validator(value):
     raise forms.ValidationError('3글자 이상 입력하세요')
 # Create your models here.
 class Book(models.Model):
-  title  = models.CharField(max_length=50)
-  author = models.CharField(max_length=50,
-                            validators=[#min_length_3_validator
-                                        MinLengthValidator(3) ])
-  publisher = models.CharField(max_length=50, blank=True, null=True)
+  title  = models.CharField(verbose_name="책제목",
+                            max_length=50)
+  author = models.CharField(verbose_name="저자",
+                    max_length=50,
+                    validators=[#min_length_3_validator
+                                MinLengthValidator(3) ])
+  publisher = models.CharField(verbose_name="출판사",
+                max_length=50, blank=True, null=True)
   publication_date = models.DateTimeField(auto_now_add=True)
-  sales     = models.IntegerField(default=1000,
+  sales     = models.IntegerField(verbose_name="판매가",
+                                  default=1000,
                                   validators=[MinValueValidator(0),
                                               MaxValueValidator(1000000)])
   ip        = models.GenericIPAddressField(blank=True, null=True)
--- source/14_django/myproject/book/views.py
+++ source/14_django/myproject/book/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 
-from .forms import BookForm
+from .forms import BookForm, BookModelForm
 from .models import Book
 from django.views.generic import ListView
 from django.views.generic import CreateView
@@ -18,7 +18,8 @@ def book_new(request):
     # request.POST의 파라미터 값을 book으로 save()
     return redirect('book:list')
   else:
-    form = BookForm()
+    #form = BookForm()
+    form = BookModelForm()
     return render(request, 'book/book_form.html',
                   {'form':form })
 
