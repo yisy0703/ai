37b002a8eaa6ed66935cb269252d8163f8d492ff
yisy0703 <yisy0703@naver.com>
2025-02-26 11:34:20 +0900

    django Form객체

--- source/14_django/myproject/book/views.py
+++ source/14_django/myproject/book/views.py
@@ -3,29 +3,37 @@ from django.shortcuts import render, redirect, get_object_or_404
 
 from .forms import BookForm, BookModelForm
 from .models import Book
+from django.views.generic import ListView
+from django.views.generic import CreateView
+from django.views.generic import UpdateView
+from django.views.generic import DeleteView
 
-def book_list(request):
-  return render(request,
-                'book/book_list.html',
-                {'book_list':Book.objects.all()})
+book_list = ListView.as_view(model=Book)
+# def book_list(request):
+#   return render(request,
+#                 'book/book_list.html',
+#                 {'book_list':Book.objects.all()})
 
-def book_new(request):
-  if request.method == 'POST':
-    # request.POST의 파라미터 값을 book으로 save()
-    form = BookModelForm(request.POST)
-    if form.is_valid():
-      # book = Book(**form.cleaned_data)
-      book = form.save(commit=False)
-      book.ip = request.META['REMOTE_ADDR'] # 요청한 client의 ip
-      book.save()
-      return redirect(book) # book.get_absolute_url 자동 호출
-      # return redirect('book:list')
-  else:
-    #form = BookForm()
-    form = BookModelForm()
-  return render(request,
-                  'book/book_form.html',
-                  {'form':form })
+book_new = CreateView.as_view(model=Book,
+                fields=['title','author','publisher','sales'])
+
+# def book_new(request):
+#   if request.method == 'POST':
+#     # request.POST의 파라미터 값을 book으로 save()
+#     form = BookModelForm(request.POST)
+#     if form.is_valid():
+#       # book = Book(**form.cleaned_data)
+#       book = form.save(commit=False)
+#       book.ip = request.META['REMOTE_ADDR'] # 요청한 client의 ip
+#       book.save()
+#       return redirect(book) # book.get_absolute_url 자동 호출
+#       # return redirect('book:list')
+#   else:
+#     #form = BookForm()
+#     form = BookModelForm()
+#   return render(request,
+#                   'book/book_form.html',
+#                   {'form':form })
 
 def book_edit(request, pk):
   book = get_object_or_404(Book, pk=pk)
