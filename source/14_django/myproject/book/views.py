9c321ef6a4986b52184f1597e06f4c6dc3f86411
yisy0703 <yisy0703@naver.com>
2025-02-26 09:36:37 +0900

    django Form객체

--- source/14_django/myproject/book/views.py
+++ source/14_django/myproject/book/views.py
@@ -7,11 +7,11 @@ from django.views.generic import CreateView
 from django.views.generic import UpdateView
 from django.views.generic import DeleteView
 
-book_list = ListView.as_view(model=Book)
-# def book_list(request):
-#   return render(request,
-#                 'book/book_list.html',
-#                 {'book_list':Book.objects.all()})
+#book_list = ListView.as_view(model=Book)
+def book_list(request):
+  return render(request,
+                'book/book_list.html',
+                {'book_list':Book.objects.all()})
 
 def book_new(request):
   if request.method == 'POST':
