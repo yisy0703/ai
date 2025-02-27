0b71df3ac98bf87de5403cf17a7411dcef0c5458
yisy0703 <yisy0703@naver.com>
2025-02-27 11:28:41 +0900

    django 파일관리

--- source/14_django/myproject/article/views.py
+++ source/14_django/myproject/article/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from article.models import Article
 
 # Create your views here.
+article_list =ListView.as_view(model=Article)
