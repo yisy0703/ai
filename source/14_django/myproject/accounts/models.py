e2342398aaed1260441b4ac0df965fe5c5fcf770
yisy0703 <yisy0703@naver.com>
2025-03-04 11:37:57 +0900

    django 사용자인증

--- source/14_django/myproject/accounts/models.py
+++ source/14_django/myproject/accounts/models.py
@@ -9,3 +9,31 @@ class Profile(models.Model):
     return "{}({}-{})".format(self.user.username,
                               self.phone_number,
                               self.address)
+
+# 이벤트 처리 : 회원가입 완료(user.save()성공) 후 가입인사를 메일 전송
+from django.db.models.signals import post_save
+from django.conf.global_settings import AUTH_USER_MODEL
+from django.core.mail import send_mail
+def on_send_mail(sender, **kwargs):
+  if kwargs['created']:
+    user = kwargs['instance']
+    title = user.username +'님 가입을 환영합니다'
+    body = user.username + '님 가입 감사합니다'
+    bodyhtml = '<h1>{}님 가입 환영합니다</h1><h2>진심진심</h2>'.format(user.username)
+    # settings.py 에 stmp 설정이 셋팅
+    send_mail(subject=title,
+              message=body,
+              from_email= 'yisy0703@naver.com',
+              recipient_list = [user.email],
+              fail_silently=False, # 메일 전송이 안 되었을 경우 아무일도 하지 않음
+              html_message=bodyhtml
+    )
+post_save.connect(on_send_mail, sender=AUTH_USER_MODEL)
+
+
+
+
+
+
+
+
--- source/14_django/myproject/myproject/settings.py
+++ source/14_django/myproject/myproject/settings.py
@@ -140,3 +140,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, '_media') # 업로드한 파일이 저장될
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.naver.com'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_SSL = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
