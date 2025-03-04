66f06b16068a0cad4642869d2e25a3649deefdf6
yisy0703 <yisy0703@naver.com>
2025-03-04 11:52:35 +0900

    django 사용자인증

--- source/14_django/myproject/accounts/views.py
+++ source/14_django/myproject/accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView, LogoutView
 from django.conf import global_settings as settings
 from .forms import SignupForm
-
+from .models import Profile
 def signup(request):
   if request.method == 'POST':
     # 회원정보 save()
@@ -20,6 +20,14 @@ def signup(request):
 login = LoginView.as_view(template_name='accounts/login_form.html')
 
 def profile(request):
-  pass
+  if request.user.__str__() != 'AnonymousUser':
+    profile = Profile.objects.get(user=request.user)
+  else:
+    profile = None
+  return render(request,
+                'accounts/profile.html',
+                {'profile':profile,
+                #  'user':request.user
+                })
 
 logout = LogoutView.as_view(next_page=settings.LOGIN_URL)
